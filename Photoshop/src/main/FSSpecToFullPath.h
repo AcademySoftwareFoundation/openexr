3b974bc99c2add825ca675d52d129814b5d01497
Paul Schneider <pauls@ilm.com>
2003-05-15 23:35:04 +0000

    Import from ILM source

---
+++ Photoshop/src/main/FSSpecToFullPath.h
@@ -0,0 +1,15 @@
+// ===========================================================================
+//	FSSpecToFullPath.h           			Part of OpenEXR
+// ===========================================================================
+
+#pragma once
+
+#include <string>
+#include "PITypes.h"
+
+extern
+short FSSpecToFullPath (const FSSpec* inFSSpec, std::string& outPath);
+
+
+
+
