9b7200baacef6022e4da49503e61243845aa2411
Drew Hess <dhess@ilm.com>
2003-10-06 22:58:45 +0000

    * Changes for Visual Studio .NET 2003.

--- OpenEXR/HalfTest/testLimits.cpp
+++ OpenEXR/HalfTest/testLimits.cpp
@@ -9,7 +9,7 @@ using namespace std;
 namespace {
 
 float
-pow (int x, int y)
+mypow (int x, int y)
 {
     bool negative = false;
 
@@ -47,48 +47,48 @@ testLimits()
     cout << "min_exponent\n";
 
     {
-	half h (pow (2, numeric_limits<half>::min_exponent - 1));
+	half h (mypow (2, numeric_limits<half>::min_exponent - 1));
 	assert (h.isNormalized());
     }
 
     {
-	half h (pow (2, numeric_limits<half>::min_exponent - 2));
+	half h (mypow (2, numeric_limits<half>::min_exponent - 2));
 	assert (h.isDenormalized());
     }
 
     cout << "max_exponent\n";
 
     {
-	half h (pow (2, numeric_limits<half>::max_exponent - 1));
+	half h (mypow (2, numeric_limits<half>::max_exponent - 1));
 	assert (h.isNormalized());
     }
 
     {
-	half h (pow (2, numeric_limits<half>::max_exponent));
+	half h (mypow (2, numeric_limits<half>::max_exponent));
 	assert (h.isInfinity());
     }
 
     cout << "min_exponent10\n";
 
     {
-	half h (pow (10, numeric_limits<half>::min_exponent10));
+	half h (mypow (10, numeric_limits<half>::min_exponent10));
 	assert (h.isNormalized());
     }
 
     {
-	half h (pow (10, numeric_limits<half>::min_exponent10 - 1));
+	half h (mypow (10, numeric_limits<half>::min_exponent10 - 1));
 	assert (h.isDenormalized());
     }
 
     cout << "max_exponent10\n";
 
     {
-	half h (pow (10, numeric_limits<half>::max_exponent10));
+	half h (mypow (10, numeric_limits<half>::max_exponent10));
 	assert (h.isNormalized());
     }
 
     {
-	half h (pow (10, numeric_limits<half>::max_exponent10 + 1));
+	half h (mypow (10, numeric_limits<half>::max_exponent10 + 1));
 	assert (h.isInfinity());
     }
 #endif
