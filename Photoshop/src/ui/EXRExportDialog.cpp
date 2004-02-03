bc4bd326fdec4b1a836e629c93e7822e3fa41563
Paul Schneider <pauls@ilm.com>
2004-02-03 01:51:23 +0000

    updated to 1.0.7 and VC7

--- Photoshop/src/ui/EXRExportDialog.cpp
+++ Photoshop/src/ui/EXRExportDialog.cpp
@@ -18,16 +18,33 @@
 //	Resource ID's
 // ---------------------------------------------------------------------------
 
-const int                   kResID_ExportDialog     =	501;
+enum
+{
+    kItem_OK                =	1,
+    kItem_Cancel,           
+    kItem_Defaults,          
+    kItem_Sep1,      
 
-const int                   kItem_OK                =	1;
-const int                   kItem_Cancel            =	2;
-const int                   kItem_Exposure          =	3;
-const int                   kItem_Gamma             =	4;
-const int                   kItem_Compression       =	5;
-const int                   kItem_Defaults          =   9;
-const int                   kItem_Premult           =   12;
+    kItem_ColorGroup,    	
+    kItem_ExposureLabel,		
+    kItem_Exposure,          
+    kItem_GammaLabel,
+    kItem_Gamma,             
+    
+    kItem_AlphaGroup,		
+    kItem_Premult,           
 
+    kItem_CompressionGroup,	
+    kItem_CompressionNone,   
+    kItem_CompressionRLE,    
+    kItem_CompressionZip,    
+    kItem_CompressionZips,   
+    kItem_CompressionPiz,   
+
+    kItem_Sep2,              
+    kItem_Text1,             
+    kItem_Text2             
+};
 
 // ---------------------------------------------------------------------------
 //	Globals - ADM makes it hard to avoid them
@@ -37,6 +54,180 @@ static ADMDialogSuite5*     sDlogSuite              =	NULL;
 static ADMItemSuite5*       sItemSuite              =	NULL;
 static ADMListSuite3*       sListSuite              =   NULL;
 static ADMEntrySuite4*      sEntrySuite             =   NULL;
+
+
+// ---------------------------------------------------------------------------
+//	ASSetRect
+// ---------------------------------------------------------------------------
+
+inline void ASSetRect (ASRect* rect, short l, short t, short r, short b)
+{
+    rect->left   = l;
+    rect->top    = t;
+    rect->right  = r;
+    rect->bottom = b;
+}
+
+
+// ---------------------------------------------------------------------------
+//	BuildDialog
+// ---------------------------------------------------------------------------
+
+static void BuildDialog (ADMDialogRef dialog)
+{
+
+	ADMItemRef		item;
+	ASRect			rect;
+
+
+	// set the dialog to the correct size
+
+	sDlogSuite->Size (dialog, 474, 295);
+
+
+	// OK button
+
+	ASSetRect (&rect, 388, 270, 468, 290);
+	item = sItemSuite->Create (dialog, kItem_OK, kADMTextPushButtonType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "OK");	
+
+
+	// cancel button
+
+	ASSetRect (&rect, 296, 270, 376, 290);
+	item = sItemSuite->Create (dialog, kItem_Cancel, kADMTextPushButtonType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "Cancel");
+
+
+	// defaults button
+
+	ASSetRect (&rect, 8, 270, 88, 290);
+	item = sItemSuite->Create (dialog, kItem_Defaults, kADMTextPushButtonType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "Defaults");
+
+
+	// separator
+
+	ASSetRect (&rect, 5, 263, 469, 265);
+	item = sItemSuite->Create (dialog, kItem_Sep1, 	kADMFrameType, &rect, NULL, NULL, 0);
+	sItemSuite->SetItemStyle (item, kADMEtchedFrameStyle);
+	
+
+    // exposure label
+
+    ASSetRect (&rect, 22, 140, 115, 160);
+    item = sItemSuite->Create (dialog, kItem_ExposureLabel, kADMTextStaticType, &rect, NULL, NULL, 0);
+    sItemSuite->SetText (item, "Exposure:");
+    sItemSuite->SetJustify (item, kADMRightJustify);
+
+
+    // exposure control
+
+	ASSetRect (&rect, 120, 140, 220, 160);
+	item = sItemSuite->Create (dialog, kItem_Exposure, kADMSpinEditType, &rect, NULL, NULL, 0);
+	
+
+    // gamma label
+
+    ASSetRect (&rect, 22, 165, 115, 185);
+    item = sItemSuite->Create (dialog, kItem_GammaLabel, kADMTextStaticType, &rect, NULL, NULL, 0);
+    sItemSuite->SetText (item, "Gamma:");
+    sItemSuite->SetJustify (item, kADMRightJustify);
+
+
+	// gamma control
+
+	ASSetRect (&rect, 120, 165, 220, 185);
+	item = sItemSuite->Create (dialog, kItem_Gamma, kADMSpinEditType, &rect, NULL, NULL, 0);
+
+
+	// color group
+
+	ASSetRect (&rect, 12, 113, 268, 200);
+	item = sItemSuite->Create (dialog, kItem_ColorGroup, kADMFrameType, &rect, NULL, NULL, 0);
+	sItemSuite->SetItemStyle (item, kADMEtchedFrameStyle);
+	sItemSuite->SetText (item, "Color Settings:");
+
+
+	// premult checkbox
+
+	ASSetRect (&rect, 65, 225, 250, 245);
+	item = sItemSuite->Create (dialog, kItem_Premult, kADMTextCheckBoxType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "Premultiply");
+
+
+	// alpha group
+	
+	ASSetRect (&rect, 12, 205, 268, 255);
+	item = sItemSuite->Create (dialog, kItem_AlphaGroup, kADMFrameType, &rect, NULL, NULL, 0);
+	sItemSuite->SetItemStyle (item, kADMEtchedFrameStyle);
+	sItemSuite->SetText (item, "Alpha:");
+
+
+	// compression choices
+
+	ASSetRect (&rect, 300, 134, 444, 154);
+	item = sItemSuite->Create (dialog, kItem_CompressionNone, kADMTextRadioButtonType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "None");
+
+	ASSetRect (&rect, 300, 154, 444, 174);
+	item = sItemSuite->Create (dialog, kItem_CompressionRLE, kADMTextRadioButtonType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "RLE");
+
+	ASSetRect (&rect, 300, 174, 444, 194);
+	item = sItemSuite->Create (dialog, kItem_CompressionZips, kADMTextRadioButtonType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "Zip");
+
+	ASSetRect (&rect, 300, 194, 444, 214);
+	item = sItemSuite->Create (dialog, kItem_CompressionZip, kADMTextRadioButtonType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "Zip (multi-scanline)");
+
+	ASSetRect (&rect, 300, 214, 444, 234);
+	item = sItemSuite->Create (dialog, kItem_CompressionPiz, kADMTextRadioButtonType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "Piz");
+
+
+	// compression group
+
+	ASSetRect (&rect, 288, 113, 464, 255);
+	item = sItemSuite->Create (dialog, kItem_CompressionGroup, kADMFrameType, &rect, NULL, NULL, 0);
+	sItemSuite->SetItemStyle (item, kADMEtchedFrameStyle);
+	sItemSuite->SetText (item, "Compression:");		
+
+
+	// separator
+
+	ASSetRect (&rect, 5, 106, 469, 108);
+	item = sItemSuite->Create (dialog, kItem_Sep2, kADMFrameType, &rect, NULL, NULL, 0);
+	sItemSuite->SetItemStyle (item, kADMEtchedFrameStyle);
+	
+
+	// some text
+
+	ASSetRect (&rect, 24, 8, 450, 44);
+	item = sItemSuite->Create (dialog, kItem_Text1, kADMTextStaticType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "The inverse of these settings will be applied to the image.");
+
+
+	// some more text
+	
+	ASSetRect (&rect, 24, 48, 450, 100);
+	item = sItemSuite->Create (dialog, kItem_Text2, kADMTextStaticType, &rect, NULL, NULL, 0);
+	sItemSuite->SetText (item, "If you use the same settings as when you loaded the image, "
+						       "it will be returned to its original colorspace.");
+
+
+	// if on Windows, swap the OK and Cancel button positions
+
+#if MSWindows
+	item = sDlogSuite->GetItem (dialog, kItem_OK);
+	sItemSuite->Move (item, 296, 270);
+
+	item = sDlogSuite->GetItem (dialog, kItem_Cancel);
+	sItemSuite->Move (item, 388, 270);
+#endif
+
+}
 
 
 // ---------------------------------------------------------------------------
@@ -63,13 +254,23 @@ static void ASAPI DoDialogOK (ADMItemRef inItem, ADMNotifierRef inNotifier)
     item = sDlogSuite->GetItem (dialog, kItem_Premult);
     globals->premult = sItemSuite->GetIntValue (item);
 
-	item  = sDlogSuite->GetItem (dialog, kItem_Compression);
-	list  = sItemSuite->GetList (item);
-	entry = sListSuite->GetActiveEntry (list);
-	globals->outputCompression = (Imf::Compression) sEntrySuite->GetIndex (entry);
-	
-	
-	// call default handler
+    item = sDlogSuite->GetItem (dialog, kItem_CompressionNone);
+	if (sItemSuite->GetBooleanValue (item)) globals->outputCompression = Imf::NO_COMPRESSION;
+
+	item = sDlogSuite->GetItem (dialog, kItem_CompressionRLE);
+	if (sItemSuite->GetBooleanValue (item)) globals->outputCompression = Imf::RLE_COMPRESSION;
+
+	item = sDlogSuite->GetItem (dialog, kItem_CompressionZip);
+	if (sItemSuite->GetBooleanValue (item)) globals->outputCompression = Imf::ZIP_COMPRESSION;
+
+	item = sDlogSuite->GetItem (dialog, kItem_CompressionZips);
+	if (sItemSuite->GetBooleanValue (item)) globals->outputCompression = Imf::ZIPS_COMPRESSION;
+
+	item = sDlogSuite->GetItem (dialog, kItem_CompressionPiz);
+	if (sItemSuite->GetBooleanValue (item)) globals->outputCompression = Imf::PIZ_COMPRESSION;	
+
+    
+    // call default handler
 	
 	sItemSuite->DefaultNotify (inItem, inNotifier);
 }
@@ -91,18 +292,28 @@ static void ASAPI DoDialogDefaults (ADMItemRef inItem, ADMNotifierRef inNotifier
 	// set control values
 	
 	item = sDlogSuite->GetItem (dialog, kItem_Exposure);
-	sItemSuite->SetFloatValue (item, 0.0);
+	sItemSuite->SetFloatValue (item, 0.0f);
 
 	item = sDlogSuite->GetItem (dialog, kItem_Gamma);
-	sItemSuite->SetFloatValue (item, 2.2);
+	sItemSuite->SetFloatValue (item, 2.2f);
 	
 	item = sDlogSuite->GetItem (dialog, kItem_Premult);
     sItemSuite->SetIntValue (item, true);
 
-	item  = sDlogSuite->GetItem (dialog, kItem_Compression);
-	list  = sItemSuite->GetList (item);
-	entry = sListSuite->GetEntry (list, Imf::PIZ_COMPRESSION);
-	sEntrySuite->Select (entry, true);
+	item  = sDlogSuite->GetItem (dialog, kItem_CompressionNone);
+	sItemSuite->SetBooleanValue (item, false);
+
+	item  = sDlogSuite->GetItem (dialog, kItem_CompressionRLE);
+	sItemSuite->SetBooleanValue (item, false);
+
+	item  = sDlogSuite->GetItem (dialog, kItem_CompressionZip);
+	sItemSuite->SetBooleanValue (item, false);
+
+	item  = sDlogSuite->GetItem (dialog, kItem_CompressionZips);
+	sItemSuite->SetBooleanValue (item, false);
+
+	item  = sDlogSuite->GetItem (dialog, kItem_CompressionPiz);
+	sItemSuite->SetBooleanValue (item, true);
 }
 
 
@@ -121,6 +332,11 @@ static ASErr ASAPI DoDialogInit (ADMDialogRef dialog)
 	int				c		=	(int) globals->outputCompression;
 	
 	
+    // create UI elements
+
+    BuildDialog (dialog);
+
+
 	// set dialog title
 	
 	sDlogSuite->SetText (dialog, "EXR Export Settings");
@@ -139,19 +355,23 @@ static ASErr ASAPI DoDialogInit (ADMDialogRef dialog)
 	item = sDlogSuite->GetItem (dialog, kItem_Premult);
     sItemSuite->SetIntValue (item, globals->premult);
 
-	item  = sDlogSuite->GetItem (dialog, kItem_Compression);
-	list  = sItemSuite->GetList (item);
+    item = sDlogSuite->GetItem (dialog, kItem_CompressionNone);
+	sItemSuite->SetBooleanValue (item, globals->outputCompression == Imf::NO_COMPRESSION);
+
+	item = sDlogSuite->GetItem (dialog, kItem_CompressionRLE);
+	sItemSuite->SetBooleanValue (item, globals->outputCompression == Imf::RLE_COMPRESSION);
 	
-	for (int i = 0; i < 5; ++i)
-	{
-		entry = sListSuite->InsertEntry (list, i);
-		sEntrySuite->SetText (entry, compressionNames[i]);
-		sEntrySuite->SetID   (entry, i);
-		sEntrySuite->Select  (entry, (i == c));
-	}
+	item = sDlogSuite->GetItem (dialog, kItem_CompressionZip);
+	sItemSuite->SetBooleanValue (item, globals->outputCompression == Imf::ZIP_COMPRESSION);
+
+	item = sDlogSuite->GetItem (dialog, kItem_CompressionZips);
+	sItemSuite->SetBooleanValue (item, globals->outputCompression == Imf::ZIPS_COMPRESSION);
+
+	item = sDlogSuite->GetItem (dialog, kItem_CompressionPiz);
+	sItemSuite->SetBooleanValue (item, globals->outputCompression == Imf::PIZ_COMPRESSION);
 		
-	
-	// set "OK" callback
+
+    // set "OK" callback
 	
 	item = sDlogSuite->GetItem (dialog, kItem_OK);
 	sItemSuite->SetUserData (item, dialog);
@@ -191,7 +411,7 @@ bool EXRExportDialog (GPtr ioGlobals, SPBasicSuite* inSPBasic, void* inPluginRef
     {
         item = sDlogSuite->Modal ((SPPluginRef) inPluginRef, 
 		                          "EXR Export Settings", 
-		                          kResID_ExportDialog, 
+		                          0, 
 		                          kADMModalDialogStyle, 
 		                          DoDialogInit, 
 		                          ioGlobals, 
@@ -230,5 +450,4 @@ bool EXRExportDialog (GPtr ioGlobals, SPBasicSuite* inSPBasic, void* inPluginRef
 
     return (item == kItem_OK);
 }
-
 
