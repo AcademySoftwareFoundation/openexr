b5bef66480f6fb30757ffffa1845612d27aba3f7
Drew Hess <dhess@ilm.com>
2004-03-23 04:00:24 +0000

    * Add some new options:
            -1        sets exposure and knee sliders so that pixel
                      value 0.0 becomes black, and 1.0 becomes white
    
            -n        normalizes the pixels so that the smallest
                      value becomes 0.0 and the largest value
                      becomes 1.0
    
            -c        display a given channel name
    
            -A        same as -c A -1 (displays alpha)
    
            -Z        same as -c Z -n (displays depth)
    
      (Florian Kainz)

--- OpenEXR/exrdisplay/loadImage.cpp
+++ OpenEXR/exrdisplay/loadImage.cpp
@@ -1,37 +1,40 @@
-///////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (c) 2004, Industrial Light & Magic, a division of Lucas
-// Digital Ltd. LLC
-// 
-// All rights reserved.
-// 
+// Copyright (c) 2004, Industrial Light & Magic, a division of Lucasfilm
+// Entertainment Company Ltd.  Portions contributed and copyright held by
+// others as indicated.  All rights reserved.
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-// *       Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-// *       Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-// *       Neither the name of Industrial Light & Magic nor the names of
-// its contributors may be used to endorse or promote products derived
-// from this software without specific prior written permission. 
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-///////////////////////////////////////////////////////////////////////////
-
+//     * Redistributions of source code must retain the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided with
+//       the distribution.
+//
+//     * Neither the name of Industrial Light & Magic nor the names of
+//       any other contributors to this software may be used to endorse or
+//       promote products derived from this software without specific prior
+//       written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////
 
 //----------------------------------------------------------------------------
 //
@@ -42,10 +45,14 @@
 #include <loadImage.h>
 #include <ImfRgbaFile.h>
 #include <ImfTiledRgbaFile.h>
+#include <ImfInputFile.h>
+#include <ImfTiledInputFile.h>
 #include <ImfPreviewImage.h>
+#include <ImfChannelList.h>
 
 using namespace Imath;
 using namespace Imf;
+using namespace std;
 
 
 void
@@ -75,7 +82,7 @@ loadImage (const char fileName[],
     {
 	in.readPixels (dataWindow.min.y, dataWindow.max.y);
     }
-    catch (const std::exception &e)
+    catch (const exception &e)
     {
 	//
 	// If some of the pixels in the file cannot be read,
@@ -83,7 +90,7 @@ loadImage (const char fileName[],
 	// to the caller.
 	//
 
-	std::cerr << e.what() << std::endl;
+	cerr << e.what() << endl;
     }
 }
 
@@ -142,7 +149,7 @@ loadTiledImage (const char fileName[],
 		    in.readTile (x, y, lx, ly);
 	}
     }
-    catch (const std::exception &e)
+    catch (const exception &e)
     {
 	//
 	// If some of the tiles in the file cannot be read,
@@ -150,7 +157,7 @@ loadTiledImage (const char fileName[],
 	// to the caller.
 	//
 
-	std::cerr << e.what() << std::endl;
+	cerr << e.what() << endl;
     }
 }
 
@@ -194,5 +201,180 @@ loadPreviewImage (const char fileName[],
 	p.g = 2.f * pow (q.g / 255.f, 2.2f);
 	p.b = 2.f * pow (q.b / 255.f, 2.2f);
 	p.a = q.a / 255.f;
+    }
+}
+
+
+void
+loadImageChannel (const char fileName[],
+		  const char channelName[],
+	          Box2i &displayWindow,
+	          Box2i &dataWindow,
+		  float &pixelAspectRatio,
+		  Array<Rgba> &pixels)
+{
+    InputFile in (fileName);
+
+    displayWindow = in.header().displayWindow();
+    dataWindow = in.header().dataWindow();
+    pixelAspectRatio = in.header().pixelAspectRatio();
+
+    int dw, dh, dx, dy;
+
+    dw = dataWindow.max.x - dataWindow.min.x + 1;
+    dh = dataWindow.max.y - dataWindow.min.y + 1;
+    dx = dataWindow.min.x;
+    dy = dataWindow.min.y;
+
+    pixels.resizeErase (dw * dh);
+
+    for (int i = 0; i < dw * dh; ++i)
+    {
+	pixels[i].r = half::qNan();
+	pixels[i].g = half::qNan();
+	pixels[i].b = half::qNan();
+    }
+
+    if (const Channel *ch = in.header().channels().findChannel (channelName))
+    {
+	FrameBuffer fb;
+
+	fb.insert (channelName,
+		   Slice (HALF,
+			  (char *) &pixels[-dx - dy * dw].g,
+			  sizeof (Rgba) * ch->xSampling,
+			  sizeof (Rgba) * ch->ySampling * dw,
+			  ch->xSampling,
+			  ch->ySampling));
+
+	in.setFrameBuffer (fb);
+    }
+    else
+    {
+	cerr << "Image file \"" << fileName << "\" has no "
+		"channel named \"" << channelName << "\"." << endl;
+	return;
+    }
+
+    try
+    {
+	in.readPixels (dataWindow.min.y, dataWindow.max.y);
+    }
+    catch (const exception &e)
+    {
+	//
+	// If some of the pixels in the file cannot be read,
+	// print an error message, and return a partial image
+	// to the caller.
+	//
+
+	cerr << e.what() << endl;
+    }
+
+    for (int i = 0; i < dw * dh; ++i)
+    {
+	pixels[i].r = pixels[i].g;
+	pixels[i].b = pixels[i].g;
+    }
+}
+
+
+void
+loadTiledImageChannel (const char fileName[],
+		       const char channelName[],
+		       int lx,
+		       int ly,
+		       Box2i &displayWindow,
+		       Box2i &dataWindow,
+		       float &pixelAspectRatio,
+		       Array<Rgba> &pixels)
+{
+    TiledInputFile in (fileName);
+
+    if (!in.isValidLevel (lx, ly))
+    {
+	THROW (Iex::InputExc, "Level (" << lx << ", " << ly << ") does "
+			      "not exist in file " << fileName << ".");
+    }
+
+    dataWindow = in.dataWindowForLevel (lx, ly);
+    displayWindow = dataWindow;
+    pixelAspectRatio = in.header().pixelAspectRatio();
+
+    int dw, dh, dx, dy;
+
+    dw = dataWindow.max.x - dataWindow.min.x + 1;
+    dh = dataWindow.max.y - dataWindow.min.y + 1;
+    dx = dataWindow.min.x;
+    dy = dataWindow.min.y;
+
+    pixels.resizeErase (dw * dh);
+
+    for (int i = 0; i < dw * dh; ++i)
+    {
+	pixels[i].r = half::qNan();
+	pixels[i].g = half::qNan();
+	pixels[i].b = half::qNan();
+    }
+
+    if (const Channel *ch = in.header().channels().findChannel (channelName))
+    {
+	FrameBuffer fb;
+
+	fb.insert (channelName,
+		   Slice (HALF,
+			  (char *) &pixels[-dx - dy * dw].g,
+			  sizeof (Rgba) * ch->xSampling,
+			  sizeof (Rgba) * ch->ySampling * dw,
+			  ch->xSampling,
+			  ch->ySampling));
+
+	in.setFrameBuffer (fb);
+    }
+    else
+    {
+	cerr << "Image file \"" << fileName << "\" has no "
+		"channel named \"" << channelName << "\"." << endl;
+	return;
+    }
+
+    try
+    {
+	int tx = in.numXTiles (lx);
+	int ty = in.numYTiles (ly);
+
+	//
+	// For maximum speed, try to read the tiles in
+	// the same order as they are stored in the file.
+	//
+
+	if (in.header().lineOrder() == INCREASING_Y)
+	{
+	    for (int y = 0; y < ty; ++y)
+		for (int x = 0; x < tx; ++x)
+		    in.readTile (x, y, lx, ly);
+	}
+	else
+	{
+	    for (int y = ty - 1; y >= 0; --y)
+		for (int x = 0; x < tx; ++x)
+		    in.readTile (x, y, lx, ly);
+	}
+    }
+    catch (const exception &e)
+    {
+	//
+	// If some of the tiles in the file cannot be read,
+	// print an error message, and return a partial image
+	// to the caller.
+	//
+
+	cerr << e.what() << endl;
+    }
+
+    for (int i = 0; i < dw * dh; ++i)
+    {
+	pixels[i].r = pixels[i].g;
+	pixels[i].b = pixels[i].g;
     }
 }
