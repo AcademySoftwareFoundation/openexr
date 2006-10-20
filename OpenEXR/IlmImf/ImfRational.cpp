5ed7880fcb7bdbf074990404ccdac306ccde7271
Florian Kainz <kainz@ilm.com>
2006-10-20 00:25:36 +0000

    Better double-to-Rational conversion routine, provided by Greg Ward.

--- OpenEXR/IlmImf/ImfRational.cpp
+++ OpenEXR/IlmImf/ImfRational.cpp
@@ -36,11 +36,57 @@
 //
 //	Rational numbers
 //
+//	The the double-to-Rational conversion code
+//	below was contributed to OpenEXR by Greg Ward.
+//
 //-----------------------------------------------------------------------------
 
 #include <ImfRational.h>
+#include <cmath>
+
+using namespace std;
 
 namespace Imf {
+namespace {
+
+double
+frac (double x, double e)
+{
+    return x - floor (x + e);
+}
+
+
+double
+square (double x)
+{
+    return x * x;
+}
+
+
+double
+denom (double x, double e)
+{
+    if (e > frac (x, e))
+    {
+        return 1;
+    }
+    else
+    {
+	double r = frac (1 / x, e);
+	
+        if (e > r)
+        {
+            return floor (1 / x + e);
+        }
+        else
+        {
+            return denom (frac (1 / r, e), e / square (x * r)) +
+                   floor (1 / x + e) * denom (frac (1 / x, e), e / square (x));
+        }
+    }
+}
+
+} // namespace
 
 
 Rational::Rational (double x)
@@ -70,28 +116,9 @@ Rational::Rational (double x)
 	return;
     }
 
-    d = 1;
-    n = int (x * d + 0.5);
-
-    while (n < (1 << 30) && d < (1U << 31))
-    {
-	d <<= 1;
-	n = int (x * d + 0.5);
-    }
-
-    if (n == 0)
-    {
-	d = 1;		// zero
-	return;
-    }
-
-    while (!(n & 1) && d > 1)
-    {
-	n >>= 1;
-	d >>= 1;
-    }
-    
-    n *= sign;
+    double e = (x < 1? 1: x) / (1U << 30);
+    d = (unsigned int) denom (x, e);
+    n = sign * (int) floor (x * d + 0.5);
 }
 
 
