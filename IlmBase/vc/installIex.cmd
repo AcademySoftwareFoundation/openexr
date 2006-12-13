9196b36da84d973091c1fd7f88daccdec29eb6a2
Florian Kainz <kainz@ilm.com>
2006-12-13 22:48:31 +0000

    Adding .cvsignore files.

--- IlmBase/vc/installIex.cmd
+++ IlmBase/vc/installIex.cmd
@@ -1,7 +1,7 @@
 @echo off
 set src=..\..\..\..\Iex
 cd %src%
-set instpath=..\..\Deploy\include
+set instpath=..\..\..\Deploy\include
 mkdir %instpath%
 copy *.h %instpath%
 
--- IlmBase/vc/installImath.cmd
+++ IlmBase/vc/installImath.cmd
@@ -1,7 +1,7 @@
 @echo off
 set src=..\..\..\..\Imath
 cd %src%
-set instpath=..\..\Deploy\include
+set instpath=..\..\..\Deploy\include
 mkdir %instpath%
 copy *.h %instpath%
 
