b71e1cba4c23bfdc985931186521c5809d747bd0
Florian Kainz <kainz@ilm.com>
2010-06-24 02:45:22 +0000

    Added new atttribute types:     M33dAttribute   3x3 double-precision matrix     M44dAttribute   4x4 double-precision matrix     V2d             2D double-precision vector     V3d             3D double-precision vector

--- OpenEXR/IlmImf/ImfMatrixAttribute.cpp
+++ OpenEXR/IlmImf/ImfMatrixAttribute.cpp
@@ -37,7 +37,9 @@
 //-----------------------------------------------------------------------------
 //
 //	class M33fAttribute
+//	class M33dAttribute
 //	class M44fAttribute
+//	class M44dAttribute
 //
 //-----------------------------------------------------------------------------
 
@@ -93,6 +95,50 @@ M33fAttribute::readValueFrom (IStream &is, int size, int version)
 
 template <>
 const char *
+M33dAttribute::staticTypeName ()
+{
+    return "m33d";
+}
+
+
+template <>
+void
+M33dAttribute::writeValueTo (OStream &os, int version) const
+{
+    Xdr::write <StreamIO> (os, _value[0][0]);
+    Xdr::write <StreamIO> (os, _value[0][1]);
+    Xdr::write <StreamIO> (os, _value[0][2]);
+
+    Xdr::write <StreamIO> (os, _value[1][0]);
+    Xdr::write <StreamIO> (os, _value[1][1]);
+    Xdr::write <StreamIO> (os, _value[1][2]);
+
+    Xdr::write <StreamIO> (os, _value[2][0]);
+    Xdr::write <StreamIO> (os, _value[2][1]);
+    Xdr::write <StreamIO> (os, _value[2][2]);
+}
+
+
+template <>
+void
+M33dAttribute::readValueFrom (IStream &is, int size, int version)
+{
+    Xdr::read <StreamIO> (is, _value[0][0]);
+    Xdr::read <StreamIO> (is, _value[0][1]);
+    Xdr::read <StreamIO> (is, _value[0][2]);
+
+    Xdr::read <StreamIO> (is, _value[1][0]);
+    Xdr::read <StreamIO> (is, _value[1][1]);
+    Xdr::read <StreamIO> (is, _value[1][2]);
+
+    Xdr::read <StreamIO> (is, _value[2][0]);
+    Xdr::read <StreamIO> (is, _value[2][1]);
+    Xdr::read <StreamIO> (is, _value[2][2]);
+}
+
+
+template <>
+const char *
 M44fAttribute::staticTypeName ()
 {
     return "m44f";
@@ -128,6 +174,66 @@ M44fAttribute::writeValueTo (OStream &os, int version) const
 template <>
 void
 M44fAttribute::readValueFrom (IStream &is, int size, int version)
+{
+    Xdr::read <StreamIO> (is, _value[0][0]);
+    Xdr::read <StreamIO> (is, _value[0][1]);
+    Xdr::read <StreamIO> (is, _value[0][2]);
+    Xdr::read <StreamIO> (is, _value[0][3]);
+
+    Xdr::read <StreamIO> (is, _value[1][0]);
+    Xdr::read <StreamIO> (is, _value[1][1]);
+    Xdr::read <StreamIO> (is, _value[1][2]);
+    Xdr::read <StreamIO> (is, _value[1][3]);
+
+    Xdr::read <StreamIO> (is, _value[2][0]);
+    Xdr::read <StreamIO> (is, _value[2][1]);
+    Xdr::read <StreamIO> (is, _value[2][2]);
+    Xdr::read <StreamIO> (is, _value[2][3]);
+
+    Xdr::read <StreamIO> (is, _value[3][0]);
+    Xdr::read <StreamIO> (is, _value[3][1]);
+    Xdr::read <StreamIO> (is, _value[3][2]);
+    Xdr::read <StreamIO> (is, _value[3][3]);
+}
+
+
+template <>
+const char *
+M44dAttribute::staticTypeName ()
+{
+    return "m44d";
+}
+
+
+template <>
+void
+M44dAttribute::writeValueTo (OStream &os, int version) const
+{
+    Xdr::write <StreamIO> (os, _value[0][0]);
+    Xdr::write <StreamIO> (os, _value[0][1]);
+    Xdr::write <StreamIO> (os, _value[0][2]);
+    Xdr::write <StreamIO> (os, _value[0][3]);
+
+    Xdr::write <StreamIO> (os, _value[1][0]);
+    Xdr::write <StreamIO> (os, _value[1][1]);
+    Xdr::write <StreamIO> (os, _value[1][2]);
+    Xdr::write <StreamIO> (os, _value[1][3]);
+
+    Xdr::write <StreamIO> (os, _value[2][0]);
+    Xdr::write <StreamIO> (os, _value[2][1]);
+    Xdr::write <StreamIO> (os, _value[2][2]);
+    Xdr::write <StreamIO> (os, _value[2][3]);
+
+    Xdr::write <StreamIO> (os, _value[3][0]);
+    Xdr::write <StreamIO> (os, _value[3][1]);
+    Xdr::write <StreamIO> (os, _value[3][2]);
+    Xdr::write <StreamIO> (os, _value[3][3]);
+}
+
+
+template <>
+void
+M44dAttribute::readValueFrom (IStream &is, int size, int version)
 {
     Xdr::read <StreamIO> (is, _value[0][0]);
     Xdr::read <StreamIO> (is, _value[0][1]);
