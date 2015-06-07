d25002208ca92272182d759825120e2bbb8d5556
xantares <xantares09@hotmail.com>
2015-06-07 18:58:34 +0200

    fix cast

--- OpenEXR/IlmImf/ImfOptimizedPixelReading.h
+++ OpenEXR/IlmImf/ImfOptimizedPixelReading.h
@@ -70,7 +70,7 @@ EXR_FORCEINLINE
 bool
 isPointerSSEAligned (const void* EXR_RESTRICT pPointer)
 {
-    unsigned long trailingBits = ((unsigned long)pPointer) & 15;
+    intptr_t trailingBits = ((intptr_t)pPointer) & 15;
     return trailingBits == 0;
 }
 
