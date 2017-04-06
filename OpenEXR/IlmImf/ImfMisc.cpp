2055e0507b9235ad2f2513368d4eacff1c48a276
John Loy <jloy@pixar.com>
2017-04-06 12:20:30 -0700

    Compute sample locations directly in Imf::readPerDeepLineTable.
    By changing the function to iterate over sample locations directly instead of
    discarding unsampled pixel positions, we can avoid computing a lot of modulos
    (more than one per pixel.)  Even on modern x86 processors, idiv is a
    relatively expensive instruction.  Though it may appear like this optimization
    could be performed by a sufficiently sophisticated compiler, gcc 4.8 does not
    get there (even at -O3.)

--- OpenEXR/IlmImf/ImfMisc.cpp
+++ OpenEXR/IlmImf/ImfMisc.cpp
@@ -132,6 +132,18 @@ bytesPerLineTable (const Header &header,
     return maxBytesPerLine;
 }
 
+static int
+roundToNextMultiple(int n, int d)
+{
+    return ((n + d - 1) / d) * d;
+}
+
+static int
+roundToPrevMultiple(int n, int d)
+{
+    return (n / d) * d;
+}
+
 size_t
 bytesPerDeepLineTable (const Header &header,
                        int minY, int maxY,
@@ -147,22 +159,33 @@ bytesPerDeepLineTable (const Header &header,
          c != channels.end();
          ++c)
     {
-        const int ySampling = c.channel().ySampling;
-        const int xSampling = c.channel().xSampling;
+        const int ySampling = abs(c.channel().ySampling);
+        const int xSampling = abs(c.channel().xSampling);
         const int pixelSize = pixelTypeSize (c.channel().type);
 
-        for (int y = minY; y <= maxY; ++y)
-            if (modp (y, ySampling) == 0)
+        // Here we transform from the domain over all pixels into the domain
+        // of actual samples.  We want to sample points in [minY, maxY] where
+        // (y % ySampling) == 0.  However, doing this by rejecting samples
+        // requires O(height*width) modulo computations, which were a
+        // significant bottleneck in the previous implementation of this
+        // function.  For the low, low price of 4 divisions per channel, we
+        // can tighten the y & x ranges to the least and greatest roots of the
+        // sampling function and then stride by the sampling rate.
+        const int sampleMinY = roundToNextMultiple(minY, ySampling);
+        const int sampleMaxY = roundToPrevMultiple(maxY, ySampling);
+        const int sampleMinX = roundToNextMultiple(dataWindow.min.x, xSampling);
+        const int sampleMaxX = roundToPrevMultiple(dataWindow.max.x, xSampling);
+
+        for (int y = sampleMinY; y <= sampleMaxY; y+=ySampling)
+        {
+            int nBytes = 0;
+            for (int x = sampleMinX; x <= sampleMaxX; x += xSampling)
             {
-                int nBytes = 0;
-                for (int x = dataWindow.min.x; x <= dataWindow.max.x; x++)
-                {
-                    if (modp (x, xSampling) == 0)
-                        nBytes += pixelSize *
-                                  sampleCount(base, xStride, yStride, x, y);
-                }
-                bytesPerLine[y - dataWindow.min.y] += nBytes;
+                nBytes += pixelSize *
+                          sampleCount(base, xStride, yStride, x, y);
             }
+            bytesPerLine[y - dataWindow.min.y] += nBytes;
+        }
     }
 
     size_t maxBytesPerLine = 0;
