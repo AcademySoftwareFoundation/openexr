dd867668c4c63d23c034cc2ea8f2352451e8554d
Kimball Thurston <kdt3rd@gmail.com>
2018-07-29 10:44:17 +1200

    make code more amenable to compiling with mingw for cross-compiling
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- OpenEXR/IlmImf/ImfOptimizedPixelReading.h
+++ OpenEXR/IlmImf/ImfOptimizedPixelReading.h
@@ -70,7 +70,7 @@ EXR_FORCEINLINE
 bool
 isPointerSSEAligned (const void* EXR_RESTRICT pPointer)
 {
-    unsigned long trailingBits = ((unsigned long)pPointer) & 15;
+    uintptr_t trailingBits = ((uintptr_t)pPointer) & 15;
     return trailingBits == 0;
 }
 
