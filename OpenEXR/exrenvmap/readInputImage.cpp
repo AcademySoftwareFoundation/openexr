4aa6a4e0fcd52b220c71807307b9139966c3644c
Kimball Thurston <kdt3rd@gmail.com>
2019-06-27 23:36:09 +1200

    Fix part of #232, issue with pointer overflows
    This addresses pointer overflow in exr2aces with large datawindow
    offsets. It also fixes similar issues in exrenvmap and exrmakepreview.
    
    This addresses the crashes in CVE-2017-9111, CVE-2017-9113,
    CVE-2017-9115
    
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- OpenEXR/exrenvmap/readInputImage.cpp
+++ OpenEXR/exrenvmap/readInputImage.cpp
@@ -194,7 +194,7 @@ readSixImages (const char inFileName[],
                    "from the data window of other cube faces.");
         }
 
-        in.setFrameBuffer (pixels - dw.min.x - dw.min.y * w, 1, w);
+        in.setFrameBuffer (pixels - intptr_t( dw.min.x ) - intptr_t( dw.min.y ) * intptr_t( w ), 1, w);
         in.readPixels (dw.min.y, dw.max.y);
 
         pixels += w * h;
