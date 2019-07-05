55c1121b5447d7b00ecdf775f6ee0315f6db7d47
Peter Hillman <peterh@wetafx.co.nz>
2019-07-05 16:07:10 +1200

    Address #270: limit to INT_MAX tiles total

--- OpenEXR/IlmImf/ImfTiledMisc.cpp
+++ OpenEXR/IlmImf/ImfTiledMisc.cpp
@@ -361,20 +361,34 @@ getTiledChunkOffsetTableSize(const Header& header)
     //
     // Calculate lineOffsetSize.
     //
-    int lineOffsetSize = 0;
+    Int64 lineOffsetSize = 0;
     const TileDescription &desc = header.tileDescription();
     switch (desc.mode)
     {
         case ONE_LEVEL:
         case MIPMAP_LEVELS:
             for (int i = 0; i < numXLevels; i++)
-                lineOffsetSize += numXTiles[i] * numYTiles[i];
-            break;
+            {
+                lineOffsetSize += Int64(numXTiles[i]) * Int64(numYTiles[i]);
+                if ( lineOffsetSize > INT_MAX )
+                {
+                    throw IEX_NAMESPACE::LogicExc("Maximum number of tiles exceeded");
+                }
+            }
+           break;
         case RIPMAP_LEVELS:
             for (int i = 0; i < numXLevels; i++)
+            {
                 for (int j = 0; j < numYLevels; j++)
-                    lineOffsetSize += numXTiles[i] * numYTiles[j];
-            break;
+                {
+                     lineOffsetSize += Int64(numXTiles[i]) * Int64(numYTiles[j]);
+                     if ( lineOffsetSize > INT_MAX )
+                     {
+                        throw IEX_NAMESPACE::LogicExc("Maximum number of tiles exceeded");
+                     }
+                }
+            }
+           break;
         case NUM_LEVELMODES :
             throw IEX_NAMESPACE::LogicExc("Bad level mode getting chunk offset table size");
     }
@@ -382,7 +396,7 @@ getTiledChunkOffsetTableSize(const Header& header)
     delete[] numXTiles;
     delete[] numYTiles;
 
-    return lineOffsetSize;
+    return int(lineOffsetSize);
 }
 
 
