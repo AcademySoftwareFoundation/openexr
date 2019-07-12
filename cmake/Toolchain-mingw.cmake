1a819ba7b10f219e21e77b0913491085a3dad7f8
Kimball Thurston <kdt3rd@gmail.com>
2019-07-12 23:08:27 +1200

    First pass making cross compile work, cross compiling windows using mingw on linux
    This currently works for building using static libraries, but not yet
    tested with dlls.
    
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

---
+++ cmake/Toolchain-mingw.cmake
@@ -0,0 +1,30 @@
+#
+# This is only an example of cross compiling, but to compile for windows
+# under linux, have the relevant bits installed, and
+# validate the find path below
+#
+# Then to run, do something like
+# mkdir win32_cross
+# cd win32_cross
+# cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-mingw.cmake -DCMAKE_INSTALL_PREFIX=/tmp/win32 ..
+# or for static libs only
+# cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-mingw.cmake -DCMAKE_INSTALL_PREFIX=/tmp/win32 ..
+# make
+#
+
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_CROSSCOMPILING_EMULATOR wine)
+
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++ -static")
+set(CMAKE_SHARED_LINKER_FLAGS "-static-libgcc -static-libstdc++ -static")
+
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
