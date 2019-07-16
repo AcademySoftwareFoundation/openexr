334324dcc1804defb1d91c021d229fe8754de8c7
Peter Hillman <peterh@wetafx.co.nz>
2019-07-16 20:16:09 +1200

    don't index empty array in testMultiPartSharedAttributes

--- OpenEXR/IlmImfTest/testMultiPartSharedAttributes.cpp
+++ OpenEXR/IlmImfTest/testMultiPartSharedAttributes.cpp
@@ -152,7 +152,7 @@ testMultiPartOutputFileForExpectedFailure (const vector<Header> & headers,
     try
     {
         remove(fn.c_str());
-        MultiPartOutputFile file(fn.c_str(), &headers[0],headers.size());
+        MultiPartOutputFile file(fn.c_str(), (headers.size()>0 ? &headers[0] : NULL) , headers.size() );
         cerr << "ERROR -- " << failMessage << endl;
         assert (false);
     }
