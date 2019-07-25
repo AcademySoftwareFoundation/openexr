74504503cff86e986bac441213c403b0ba28d58f
Peter Hillman <peterh@wetafx.co.nz>
2019-07-25 19:50:11 +1200

    Fix logic for 1 pixel high/wide preview images (Fixes #493)

--- OpenEXR/exrmakepreview/makePreview.cpp
+++ OpenEXR/exrmakepreview/makePreview.cpp
@@ -120,8 +120,8 @@ generatePreview (const char inFileName[],
     previewHeight = max (int (h / (w * a) * previewWidth + .5f), 1);
     previewPixels.resizeErase (previewHeight, previewWidth);
 
-    float fx = (previewWidth  > 0)? (float (w - 1) / (previewWidth  - 1)): 1;
-    float fy = (previewHeight > 0)? (float (h - 1) / (previewHeight - 1)): 1;
+    float fx = (previewWidth  > 1)? (float (w - 1) / (previewWidth  - 1)): 1;
+    float fy = (previewHeight > 1)? (float (h - 1) / (previewHeight - 1)): 1;
     float m  = Math<float>::pow (2.f, IMATH_NAMESPACE::clamp (exposure + 2.47393f, -20.f, 20.f));
 
     for (int y = 0; y < previewHeight; ++y)
