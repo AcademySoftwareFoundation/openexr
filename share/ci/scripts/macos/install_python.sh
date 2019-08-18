1bcfc9a17dfeaff2e92d0190c95a62d58d34f3c6
Christina Tempelaar-Lietz <xlietz@gmail.com>
2019-08-18 13:40:49 -0700

    Azure updates for MacOS/Windows/Linux
    Signed-off-by: Christina Tempelaar-Lietz <xlietz@gmail.com>

--- share/ci/scripts/macos/install_python.sh
+++ share/ci/scripts/macos/install_python.sh
@@ -3,9 +3,12 @@
 set -ex
 
 PYTHON_VERSION="$1"
+PYTHON_MAJOR="$(echo ${PYTHON_VERSION} | cut -f 1 -d .)"
 
 MACOS_MAJOR="$(sw_vers -productVersion | cut -f 1 -d .)"
 MACOS_MINOR="$(sw_vers -productVersion | cut -f 2 -d .)"
+
+echo "Installing Python ${PYTHON_VERSION} Major ${PYTHON_MAJOR}"
 
 # This workaround is needed for building Python on macOS >= 10.14:
 #   https://developer.apple.com/documentation/xcode_release_notes/xcode_10_release_notes
@@ -16,9 +19,21 @@ if [[ "$MACOS_MAJOR" -gt 9 && "$MACOS_MINOR" -gt 13 ]]; then
         -target /
 fi
 
-brew install pyenv
+unset CFLAGS
+
+brew update
+brew install pyenv openssl 
+
+CFLAGS="-I/usr/local/Cellar/openssl/1.0.2s/include"
+LDFLAGS="-L/usr/local/Cellar/openssl/1.0.2s/lib"
 
 echo 'eval "$(pyenv init -)"' >> .bash_profile
 source .bash_profile
 env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install -v ${PYTHON_VERSION}
 pyenv global ${PYTHON_VERSION}
+
+if [[ ${PYTHON_MAJOR}=2 ]]; then
+    pip install numpy
+else
+    pip3 install numpy
+fi
--- share/ci/scripts/windows/install_python.ps1
+++ share/ci/scripts/windows/install_python.ps1
@@ -1,6 +1,18 @@
 $pythonVersion = $Args[0]
+$pythonMajor = ($pythonVersion -split '\.')[0]
 
-Invoke-WebRequest "https://www.python.org/ftp/python/${pythonVersion}/python-${pythonVersion}.amd64.msi" -OutFile "C:\python-${pythonVersion}.amd64.msi"
-msiexec /i "C:\python-${pythonVersion}.amd64.msi" /quiet /l* C:\_python.log TARGETDIR=C:\_python
+Write-Host "Installing python version ${pythonVersion}"
+
+if ($pythonMajor -eq "3")
+{
+    Invoke-WebRequest "https://www.python.org/ftp/python/${pythonVersion}/python-${pythonVersion}-amd64.exe" -OutFile "C:\python-${pythonVersion}-amd64.exe"
+    & "C:\python-${pythonVersion}-amd64.exe" /quiet /l* C:\_python.log TARGETDIR=C:\_python
+}
+else
+{
+    Invoke-WebRequest "https://www.python.org/ftp/python/${pythonVersion}/python-${pythonVersion}.amd64.msi" -OutFile "C:\python-${pythonVersion}-amd64.msi"
+    msiexec /i "C:\python-${pythonVersion}-amd64.msi" /quiet /l* C:\_python.log TARGETDIR=C:\_python
+}
+
 Write-Host "##vso[task.prependpath]C:\_python"
 Write-Host "##vso[task.prependpath]C:\_python\Scripts"
