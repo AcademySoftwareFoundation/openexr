eb614fa7a9f5d02e66625ba9617aafd9d4f83077
Cary Phillips <cary@ilm.com>
2020-05-01 13:23:38 -0700

    A clean version of #673: Allow the use of Pthreads with WIN32/64 builds under MinGW
    Signed-off-by: Cary Phillips <cary@ilm.com>
    Signed-off-by: smartin-13 <61707536+smartin-13@users.noreply.github.com>

--- IlmBase/IlmThread/IlmThreadMutex.h
+++ IlmBase/IlmThread/IlmThreadMutex.h
@@ -71,13 +71,13 @@
 #include "IlmThreadNamespace.h"
 
 #ifdef ILMBASE_FORCE_CXX03
-#   if defined _WIN32 || defined _WIN64
+#   if defined (_WIN32) || defined (_WIN64)
 #      ifdef NOMINMAX
 #         undef NOMINMAX
 #      endif
 #      define NOMINMAX
 #      include <windows.h>
-#   elif HAVE_PTHREAD
+#   elif defined (HAVE_PTHREAD)
 #      include <pthread.h>
 #   endif
 #else
@@ -116,9 +116,9 @@ class ILMTHREAD_EXPORT Mutex
     void	lock () const;
     void	unlock () const;
 
-    #if defined _WIN32 || defined _WIN64
+    #if (defined (_WIN32) || defined (_WIN64)) && !defined (HAVE_PTHREADS)
 	mutable CRITICAL_SECTION _mutex;
-    #elif HAVE_PTHREAD
+    #elif defined (HAVE_PTHREAD)
 	mutable pthread_mutex_t _mutex;
     #endif
 
