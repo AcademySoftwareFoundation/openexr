5f94702b582971f20d8265dfe1df6a4e58761ad2
Christina Tempelaar-Lietz <xlietz@gmail.com>
2020-05-19 06:41:54 +0100

    Added github actions CI.
    Signed-off-by: Christina Tempelaar-Lietz <xlietz@gmail.com>

--- share/ci/scripts/windows/install_zlib.ps1
+++ share/ci/scripts/windows/install_zlib.ps1
@@ -1,3 +1,5 @@
+$homeDir = (pwd)
+
 $zlibVersion = $Args[0]
 $zlibMajorMinor = [io.path]::GetFileNameWithoutExtension("$zlibVersion")
 $zlibVersionConcise = $zlibVersion -replace '[.]',''
@@ -6,7 +8,7 @@ $zlibArchive = "https://www.zlib.net/zlib${zlibVersionConcise}.zip"
 $zlibRoot = "C:\"
 $zlibBuildPath = "${zlibRoot}\zlib-${zlibVersion}"
 $zlibInstallPath = "C:\_zlib"
-$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe"
+$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe"
 
 Write-Host "Retrieving ${zlibArchive}"
 Invoke-WebRequest "${zlibArchive}" -OutFile "${zlibRoot}\zlib-${zlibVersion}.zip"
@@ -16,7 +18,7 @@ Expand-Archive "${zlibRoot}\zlib-${zlibVersion}.zip" -DestinationPath "${zlibRoo
 cd $zlibBuildPath
 mkdir _build
 cd _build
-cmake .. -G"Visual Studio 15 2017" -DCMAKE_INSTALL_PREFIX="${zlibInstallPath}"
+cmake .. -G"Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX="${zlibInstallPath}"
 
 Write-Host "Building ${zlibBuildPath}\_build\INSTALL.vcxproj" -foregroundcolor green 
 & "${msbuild}" "${zlibBuildPath}\_build\INSTALL.vcxproj" /P:Configuration=Release
@@ -25,9 +27,19 @@ cd $zlibInstallPath
 Write-Host "ls ${zlibInstallPath}"
 & ls
 
-cd 
-& ls
+Write-Host "ls bin"
+& ls bin
+Write-Host "ls include"
+& ls include
+Write-Host "ls lib"
+& ls lib
+Write-Host "ls share"
+& ls share
+
+cd $homeDir
 
 Write-Host "##vso[task.prependpath]${zlibInstallPath}"
 Write-Host "##vso[task.prependpath]${zlibInstallPath}\bin"
+Write-Host "##vso[task.prependpath]${zlibInstallPath}\include"
+Write-Host "##vso[task.prependpath]${zlibInstallPath}\lib"
 
