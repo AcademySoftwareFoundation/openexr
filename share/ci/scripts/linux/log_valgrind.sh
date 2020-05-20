89314e9f809110757304f7ccdf54d58a19d502f2
Christina Tempelaar-Lietz <xlietz@gmail.com>
2020-05-20 22:11:17 +0100

    Added Valgrind and Fuzz Test CI jobs.
    Signed-off-by: Christina Tempelaar-Lietz <xlietz@gmail.com>

---
+++ share/ci/scripts/linux/log_valgrind.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "here"
+
+set -ex
+
+LOGDIR="$1"
+
+if [[ -d ${LOGDIR}/Testing/Temporary ]]; then 
+	for log in ${LOGDIR}/Testing/Temporary/MemoryChecker.*.log; do 
+		echo "========================== " ${log##*/} " ==========================" 
+		cat $log 
+		echo 
+	done 
+else 
+	echo "Memcheck log files not found." 
+fi
