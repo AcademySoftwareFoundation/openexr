d25b928b5564da1133ceb12a05d0599341f97024
Christina Tempelaar-Lietz <xlietz@gmail.com>
2020-05-29 20:40:50 +0100

    fixing windows ci jobs.
    Signed-off-by: Christina Tempelaar-Lietz <xlietz@gmail.com>

--- share/ci/scripts/windows/install_python.ps1
+++ share/ci/scripts/windows/install_python.ps1
@@ -1,18 +1,34 @@
-$pythonVersion = $Args[0]
-$pythonMajor = ($pythonVersion -split '\.')[0]
+$homeDir = (pwd)
 
-Write-Host "Installing python version ${pythonVersion}"
+$pythonVersion = $Args[0]
+$pythonWorkingDir = $Args[1]
+
+$pythonMajor = ($pythonVersion -split '\.')[0]
+$pythonMinor = ($pythonVersion -split '\.')[1]
+$pythonRoot = "${pythonWorkingDir}\_python${pythonMajor}${pythonMinor}"
+
+Write-Host "Installing python version ${pythonVersion} ${pythonRoot}"
+
+if (-NOT (Test-Path $pythonRoot))
+{
+	New-Item -ItemType Directory $pythonRoot
+}
+
+cd $pythonRoot
 
 if ($pythonMajor -eq "3")
 {
-    Invoke-WebRequest "https://www.python.org/ftp/python/${pythonVersion}/python-${pythonVersion}-amd64.exe" -OutFile "python-${pythonVersion}-amd64.exe"
-    & "./python-${pythonVersion}-amd64.exe" /quiet /l* _python.log TARGETDIR=_python
+    Invoke-WebRequest "https://www.python.org/ftp/python/${pythonVersion}/python-${pythonVersion}.exe" -OutFile "python-${pythonVersion}-amd64.exe"
+    Invoke-Expression "./python-${pythonVersion}-amd64.exe /quiet /l* _python.log TargetDir=${pythonRoot} PrependPath=1"
 }
 else
 {
     Invoke-WebRequest "https://www.python.org/ftp/python/${pythonVersion}/python-${pythonVersion}.amd64.msi" -OutFile "python-${pythonVersion}-amd64.msi"
-    msiexec /i "python-${pythonVersion}-amd64.msi" /quiet /l* _python.log TARGETDIR=_python
+    msiexec /i "python-${pythonVersion}-amd64.msi" /quiet /l* _python.log TARGETDIR="${pythonRoot}" PrependPath=1
 }
 
-Write-Host "##vso[task.prependpath]_python"
-Write-Host "##vso[task.prependpath]_python\Scripts"
+cd $homeDir
+
+echo "::set-env name=PYTHON_ROOT::$pythonRoot"
+echo "::add-path::$pythonRoot"
+echo "::add-path::$pythonRoot/Scripts"
