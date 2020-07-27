7a7fab1b5aa8eb91239907cc598f9795431557b1
Owen Thompson <oxt3479@rit.edu>
2020-07-27 15:46:09 -0400

    Initial move of config/* subdirectories to top level cmake/*, combine IlmBase and OpenEXR into one project.
    Signed-off-by: Owen Thompson <oxt3479@rit.edu>

--- cmake/LibraryDefine.cmake
+++ cmake/LibraryDefine.cmake
@@ -3,18 +3,18 @@
 
 # NB: This function has a number if IlmBase specific names / variables
 # in it, so be careful copying...
-function(ILMBASE_DEFINE_LIBRARY libname)
+function(OPENEXR_DEFINE_LIBRARY libname)
   set(options)
   set(oneValueArgs PRIV_EXPORT CURDIR CURBINDIR)
   set(multiValueArgs SOURCES HEADERS DEPENDENCIES PRIVATE_DEPS)
-  cmake_parse_arguments(ILMBASE_CURLIB "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  cmake_parse_arguments(OPENEXR_CURLIB "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
   # only do the object library mechanism in a few cases:
   # - xcode doesn't handle "empty" targets (i.e. add_library with
   #   an object lib only)
   # - if we're not compiling both, don't add the extra layer to prevent
   #   extra compiles since we aren't doing that anyway
-  if(ILMBASE_BUILD_BOTH_STATIC_SHARED AND NOT (APPLE OR WIN32))
+  if(OPENEXR_BUILD_BOTH_STATIC_SHARED AND NOT (APPLE OR WIN32))
     set(use_objlib TRUE)
   else()
     set(use_objlib)
@@ -25,31 +25,31 @@ function(ILMBASE_DEFINE_LIBRARY libname)
   if(use_objlib)
     set(objlib ${libname}_Object)
     add_library(${objlib} OBJECT
-      ${ILMBASE_CURLIB_HEADERS}
-      ${ILMBASE_CURLIB_SOURCES})
+      ${OPENEXR_CURLIB_HEADERS}
+      ${OPENEXR_CURLIB_SOURCES})
   else()
     set(objlib ${libname})
     add_library(${objlib}
-      ${ILMBASE_CURLIB_HEADERS}
-      ${ILMBASE_CURLIB_SOURCES})
+      ${OPENEXR_CURLIB_HEADERS}
+      ${OPENEXR_CURLIB_SOURCES})
   endif()
 
   target_compile_features(${objlib} PUBLIC cxx_std_${OPENEXR_CXX_STANDARD})
-  if(ILMBASE_CURLIB_PRIV_EXPORT AND BUILD_SHARED_LIBS)
-    target_compile_definitions(${objlib} PRIVATE ${ILMBASE_CURLIB_PRIV_EXPORT})
-    if(WIN32 AND NOT ILMBASE_BUILD_BOTH_STATIC_SHARED)
+  if(OPENEXR_CURLIB_PRIV_EXPORT AND BUILD_SHARED_LIBS)
+    target_compile_definitions(${objlib} PRIVATE ${OPENEXR_CURLIB_PRIV_EXPORT})
+    if(WIN32 AND NOT OPENEXR_BUILD_BOTH_STATIC_SHARED)
       target_compile_definitions(${objlib} PUBLIC OPENEXR_DLL)
     endif()
   endif()
-  if(ILMBASE_CURLIB_CURDIR)
-    target_include_directories(${objlib} INTERFACE $<BUILD_INTERFACE:${ILMBASE_CURLIB_CURDIR}>)
+  if(OPENEXR_CURLIB_CURDIR)
+    target_include_directories(${objlib} INTERFACE $<BUILD_INTERFACE:${OPENEXR_CURLIB_CURDIR}>)
   endif()
-  if(ILMBASE_CURLIB_CURBINDIR)
-    target_include_directories(${objlib} PRIVATE $<BUILD_INTERFACE:${ILMBASE_CURLIB_CURBINDIR}>)
+  if(OPENEXR_CURLIB_CURBINDIR)
+    target_include_directories(${objlib} PRIVATE $<BUILD_INTERFACE:${OPENEXR_CURLIB_CURBINDIR}>)
   endif()
-  target_link_libraries(${objlib} PUBLIC ${PROJECT_NAME}::Config ${ILMBASE_CURLIB_DEPENDENCIES})
-  if(ILMBASE_CURLIB_PRIVATE_DEPS)
-    target_link_libraries(${objlib} PRIVATE ${ILMBASE_CURLIB_PRIVATE_DEPS})
+  target_link_libraries(${objlib} PUBLIC ${PROJECT_NAME}::Config ${OPENEXR_CURLIB_DEPENDENCIES})
+  if(OPENEXR_CURLIB_PRIVATE_DEPS)
+    target_link_libraries(${objlib} PRIVATE ${OPENEXR_CURLIB_PRIVATE_DEPS})
   endif()
   set_target_properties(${objlib} PROPERTIES
     CXX_STANDARD_REQUIRED ON
@@ -59,13 +59,13 @@ function(ILMBASE_DEFINE_LIBRARY libname)
   if (_ilmbase_extra_flags)
     target_compile_options(${objlib} PUBLIC ${_ilmbase_extra_flags})
   endif()
-  set_property(TARGET ${objlib} PROPERTY PUBLIC_HEADER ${ILMBASE_CURLIB_HEADERS})
+  set_property(TARGET ${objlib} PROPERTY PUBLIC_HEADER ${OPENEXR_CURLIB_HEADERS})
 
   if(use_objlib)
     install(TARGETS ${objlib}
       EXPORT ${PROJECT_NAME}
       PUBLIC_HEADER
-        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${ILMBASE_OUTPUT_SUBDIR}
+        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${OPENEXR_OUTPUT_SUBDIR}
     )
   endif()
 
@@ -77,12 +77,12 @@ function(ILMBASE_DEFINE_LIBRARY libname)
   endif()
   if(BUILD_SHARED_LIBS)
     set_target_properties(${libname} PROPERTIES
-      SOVERSION ${ILMBASE_SOVERSION}
-      VERSION ${ILMBASE_LIB_VERSION}
+      SOVERSION ${OPENEXR_SOVERSION}
+      VERSION ${OPENEXR_LIB_VERSION}
     )
   endif()
   set_target_properties(${libname} PROPERTIES
-      OUTPUT_NAME "${libname}${ILMBASE_LIB_SUFFIX}"
+      OUTPUT_NAME "${libname}${OPENEXR_LIB_SUFFIX}"
       RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
   )
   add_library(${PROJECT_NAME}::${libname} ALIAS ${libname})
@@ -94,10 +94,10 @@ function(ILMBASE_DEFINE_LIBRARY libname)
     ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
     INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
     PUBLIC_HEADER
-      DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${ILMBASE_OUTPUT_SUBDIR}
+      DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${OPENEXR_OUTPUT_SUBDIR}
   )
-  if(BUILD_SHARED_LIBS AND (NOT "${ILMBASE_LIB_SUFFIX}" STREQUAL "") AND NOT WIN32)
-    set(verlibname ${CMAKE_SHARED_LIBRARY_PREFIX}${libname}${ILMBASE_LIB_SUFFIX}${CMAKE_SHARED_LIBRARY_SUFFIX})
+  if(BUILD_SHARED_LIBS AND (NOT "${OPENEXR_LIB_SUFFIX}" STREQUAL "") AND NOT WIN32)
+    set(verlibname ${CMAKE_SHARED_LIBRARY_PREFIX}${libname}${OPENEXR_LIB_SUFFIX}${CMAKE_SHARED_LIBRARY_SUFFIX})
     set(baselibname ${CMAKE_SHARED_LIBRARY_PREFIX}${libname}${CMAKE_SHARED_LIBRARY_SUFFIX})
     install(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E chdir \"\$ENV\{DESTDIR\}${CMAKE_INSTALL_FULL_LIBDIR}\" ${CMAKE_COMMAND} -E create_symlink ${verlibname} ${baselibname})")
     install(CODE "message(\"-- Creating symlink in ${CMAKE_INSTALL_FULL_LIBDIR} ${baselibname} -> ${verlibname}\")")
@@ -105,7 +105,7 @@ function(ILMBASE_DEFINE_LIBRARY libname)
     set(baselibname)
   endif()
 
-  if(ILMBASE_BUILD_BOTH_STATIC_SHARED)
+  if(OPENEXR_BUILD_BOTH_STATIC_SHARED)
     if(use_objlib)
       add_library(${libname}_static STATIC $<TARGET_OBJECTS:${objlib}>)
       target_link_libraries(${libname}_static INTERFACE ${objlib})
@@ -113,17 +113,17 @@ function(ILMBASE_DEFINE_LIBRARY libname)
       # have to build multiple times... but have different flags anyway (i.e. no dll)
       target_compile_definitions(${libname} PRIVATE OPENEXR_DLL)
       set(curlib ${libname}_static)
-      add_library(${curlib} STATIC ${ILMBASE_CURLIB_SOURCES})
+      add_library(${curlib} STATIC ${OPENEXR_CURLIB_SOURCES})
       target_compile_features(${curlib} PUBLIC cxx_std_${OPENEXR_CXX_STANDARD})
-      if(ILMBASE_CURLIB_CURDIR)
-        target_include_directories(${curlib} INTERFACE $<BUILD_INTERFACE:${ILMBASE_CURLIB_CURDIR}>)
+      if(OPENEXR_CURLIB_CURDIR)
+        target_include_directories(${curlib} INTERFACE $<BUILD_INTERFACE:${OPENEXR_CURLIB_CURDIR}>)
       endif()
-      if(ILMBASE_CURLIB_CURBINDIR)
-        target_include_directories(${curlib} PRIVATE $<BUILD_INTERFACE:${ILMBASE_CURLIB_CURBINDIR}>)
+      if(OPENEXR_CURLIB_CURBINDIR)
+        target_include_directories(${curlib} PRIVATE $<BUILD_INTERFACE:${OPENEXR_CURLIB_CURBINDIR}>)
       endif()
-      target_link_libraries(${curlib} PUBLIC ${PROJECT_NAME}::Config ${ILMBASE_CURLIB_DEPENDENCIES})
-      if(ILMBASE_CURLIB_PRIVATE_DEPS)
-        target_link_libraries(${curlib} PRIVATE ${ILMBASE_CURLIB_PRIVATE_DEPS})
+      target_link_libraries(${curlib} PUBLIC ${PROJECT_NAME}::Config ${OPENEXR_CURLIB_DEPENDENCIES})
+      if(OPENEXR_CURLIB_PRIVATE_DEPS)
+        target_link_libraries(${curlib} PRIVATE ${OPENEXR_CURLIB_PRIVATE_DEPS})
       endif()
       set(curlib)
     endif()
@@ -132,9 +132,9 @@ function(ILMBASE_DEFINE_LIBRARY libname)
       CXX_STANDARD_REQUIRED ON
       CXX_EXTENSIONS OFF
       POSITION_INDEPENDENT_CODE ON
-      SOVERSION ${ILMBASE_SOVERSION}
-      VERSION ${ILMBASE_LIB_VERSION}
-      OUTPUT_NAME "${libname}${ILMBASE_LIB_SUFFIX}${ILMBASE_STATIC_LIB_SUFFIX}"
+      SOVERSION ${OPENEXR_SOVERSION}
+      VERSION ${OPENEXR_LIB_VERSION}
+      OUTPUT_NAME "${libname}${OPENEXR_LIB_SUFFIX}${OPENEXR_STATIC_LIB_SUFFIX}"
     )
     add_library(${PROJECT_NAME}::${libname}_static ALIAS ${libname}_static)
 
