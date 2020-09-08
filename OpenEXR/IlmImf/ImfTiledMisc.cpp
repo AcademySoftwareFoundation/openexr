a03aafb6bb1cd29a6cbd8d7e6434ad4c2b23b935
Peter Hillman <peterh@wetafx.co.nz>
2020-09-09 11:39:07 +1200

    use signed 64 bit instead for dataWindow calculation
    Signed-off-by: Peter Hillman <peterh@wetafx.co.nz>

--- OpenEXR/IlmImf/ImfTiledMisc.cpp
+++ OpenEXR/IlmImf/ImfTiledMisc.cpp
@@ -97,14 +97,14 @@ dataWindowForTile (const TileDescription &tileDesc,
     V2i tileMin = V2i (minX + dx * tileDesc.xSize,
 		       minY + dy * tileDesc.ySize);
 
-    Int64 tileMaxX = Int64(tileMin[0]) + tileDesc.xSize - 1;
-    Int64 tileMaxY = Int64(tileMin[1]) + tileDesc.ySize - 1;
+    int64_t tileMaxX = int64_t(tileMin[0]) + tileDesc.xSize - 1;
+    int64_t tileMaxY = int64_t(tileMin[1]) + tileDesc.ySize - 1;
 
     V2i levelMax = dataWindowForLevel
 		       (tileDesc, minX, maxX, minY, maxY, lx, ly).max;
 
-    V2i tileMax = V2i (std::min (tileMaxX, Int64(levelMax[0])),
-		   std::min (tileMaxY, Int64(levelMax[1])));
+    V2i tileMax = V2i (std::min (tileMaxX, int64_t(levelMax[0])),
+		   std::min (tileMaxY, int64_t(levelMax[1])));
 
     return Box2i (tileMin, tileMax);
 }
