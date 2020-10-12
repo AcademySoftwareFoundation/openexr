84863e117585fb9fa14586b6755e2442a3d95916
peterhillman <peterh@wetafx.co.nz>
2020-10-13 09:22:45 +1300

    Validate reconstructed chunk sizes (#848)
    Signed-off-by: Peter Hillman <peterh@wetafx.co.nz>

--- OpenEXR/IlmImf/ImfDeepScanLineInputFile.cpp
+++ OpenEXR/IlmImf/ImfDeepScanLineInputFile.cpp
@@ -344,6 +344,17 @@ reconstructLineOffsets (OPENEXR_IMF_INTERNAL_NAMESPACE::IStream &is,
             OPENEXR_IMF_INTERNAL_NAMESPACE::Xdr::read <OPENEXR_IMF_INTERNAL_NAMESPACE::StreamIO> (is, packed_offset);
             OPENEXR_IMF_INTERNAL_NAMESPACE::Xdr::read <OPENEXR_IMF_INTERNAL_NAMESPACE::StreamIO> (is, packed_sample);
             //next is unpacked sample table size - skip this too
+
+            // check for bad values to prevent overflow
+            if (packed_offset < 0 ||
+                packed_sample < 0 ||
+                (INT64_MAX-packed_offset < packed_sample ) ||
+                (INT64_MAX-(packed_offset+packed_sample) < 8 ) )
+            {
+                 throw IEX_NAMESPACE::IoExc("Invalid chunk size");
+            }
+
+
             Xdr::skip <StreamIO> (is, packed_offset+packed_sample+8);
 
             if (lineOrder == INCREASING_Y)
