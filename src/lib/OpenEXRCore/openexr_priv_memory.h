5f0355501026d9f26f9a4cb5df889c0230697935
Kimball Thurston <kdt3rd@gmail.com>
2021-01-23 15:33:04 +1300

    only gcc / clang have malloc attribute
    The attribute can have some optimization and analysis benefits, so leave
    for these, but it is only additional information, so just elide for
    other systems
    
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- src/lib/OpenEXRCore/openexr_priv_memory.h
+++ src/lib/OpenEXRCore/openexr_priv_memory.h
@@ -8,7 +8,11 @@
 
 #include "openexr_base.h"
 
-extern void *priv_alloc( size_t bytes ) __attribute__((malloc));
+#if defined(__GNUC__) || defined(__clang__)
+__attribute__((malloc))
+#endif
+extern void *priv_alloc( size_t bytes );
+
 extern void priv_free( void *ptr );
 
 #endif /* OPENEXR_PRIVATE_MEMORY_H */
