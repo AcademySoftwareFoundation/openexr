bfbd34827e4c552289493100f8847b2cde595ff4
Cary Phillips <cary@ilm.com>
2021-07-29 15:54:52 -0700

    Improve handling of #include <*intrin.h> (#1117)
    * Don't use __has_include, it may report files exist when they don't.
    
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/lib/OpenEXRCore/internal_coding.h
+++ src/lib/OpenEXRCore/internal_coding.h
@@ -24,13 +24,12 @@
 #    include <half.h>
 #endif
 
-#if defined(__has_include) && defined(__x86_64__)
-#    if __has_include(<x86intrin.h>)
-#        include <x86intrin.h>
-#    elif __has_include(<intrin.h>)
+#ifdef _WIN32
 #        include <intrin.h>
-#    endif
+#elif defined(__x86_64__)
+#        include <x86intrin.h>
 #endif
+
 #include <math.h>
 
 #ifdef __cplusplus
