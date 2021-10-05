c7bcd54497a4c1c59d33fadb1c03945c2dd0d60b
Kimball Thurston <kdt3rd@gmail.com>
2021-10-06 08:21:43 +1300

    Adjust logic so we can handle single scanline compression with image sampling
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- src/lib/OpenEXRCore/coding.c
+++ src/lib/OpenEXRCore/coding.c
@@ -254,17 +254,14 @@ internal_decode_alloc_buffer (
     size_t                               newsz)
 {
     void* curbuf = *buf;
-    if (newsz == 0)
-    {
-        EXR_PROMOTE_CONST_CONTEXT_AND_PART_OR_ERROR_NO_LOCK (
-            decode->context, decode->part_index);
 
-        return pctxt->print_error (
-            pctxt,
-            EXR_ERR_INVALID_ARGUMENT,
-            "Attempt to allocate 0 byte buffer for transcode buffer %d",
-            (int) bufid);
-    }
+    /* We might have a zero size here due to y sampling on a scanline
+     * image where there is an attempt to read that portion of the
+     * image. Just shortcut here and handle at a higher level where
+     * there is more context
+     */
+    if (newsz == 0)
+        return EXR_ERR_SUCCESS;
 
     if (!curbuf || *cursz < newsz)
     {
