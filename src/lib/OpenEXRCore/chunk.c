2ae006304d4867391f6e743307e4609b5feab571
Rémi Achard <remiachard@gmail.com>
2022-03-13 19:18:52 +0000

    Fix MSVC 2022 build
    Signed-off-by: Rémi Achard <remiachard@gmail.com>

--- src/lib/OpenEXRCore/chunk.c
+++ src/lib/OpenEXRCore/chunk.c
@@ -15,9 +15,12 @@
 /**************************************/
 
 /* for testing, we include a bunch of internal stuff into the unit tests which are in c++ */
-#if defined __has_include
-#    if __has_include(<stdatomic.h>)
-#        define EXR_HAS_STD_ATOMICS 1
+/* see internal_structs.h for details on the msvc guard. */
+#if !defined(_MSC_VER)
+#    if defined __has_include
+#        if __has_include(<stdatomic.h>)
+#            define EXR_HAS_STD_ATOMICS 1
+#        endif
 #    endif
 #endif
 
