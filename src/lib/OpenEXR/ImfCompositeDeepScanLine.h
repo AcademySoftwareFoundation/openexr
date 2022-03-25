c431d894320a863b1fd78a700c8c7385716428b9
peterhillman <peterh@wetafx.co.nz>
2022-03-25 13:09:23 +1300

    Add maximumSampleCount limit to CompositeDeepScanLine (#1230)
    * Add maximumSampleCount limit to CompositeDeepScanLine
    
    Signed-off-by: Peter Hillman <peterh@wetafx.co.nz>
    
    * maximumSampleCount checks off by default; enabled in checkOpenEXRFile
    
    Signed-off-by: Peter Hillman <peterh@wetafx.co.nz>
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/lib/OpenEXR/ImfCompositeDeepScanLine.h
+++ src/lib/OpenEXR/ImfCompositeDeepScanLine.h
@@ -39,6 +39,7 @@ OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_ENTER
 
 class IMF_EXPORT_TYPE CompositeDeepScanLine
 {
+<<<<<<< HEAD
     public:
         IMF_EXPORT
         CompositeDeepScanLine();
@@ -118,6 +119,100 @@ class IMF_EXPORT_TYPE CompositeDeepScanLine
     CompositeDeepScanLine & operator=(const CompositeDeepScanLine &) = delete;
     CompositeDeepScanLine(CompositeDeepScanLine &&) = delete;
     CompositeDeepScanLine & operator=(CompositeDeepScanLine &&) = delete;
+=======
+public:
+    IMF_EXPORT
+    CompositeDeepScanLine ();
+    IMF_EXPORT
+    virtual ~CompositeDeepScanLine ();
+
+    /// set the source data as a part
+    ///@note all parts must remain valid until after last interaction with DeepComp
+    IMF_EXPORT
+    void addSource (DeepScanLineInputPart* part);
+
+    /// set the source data as a file
+    ///@note all file must remain valid until after last interaction with DeepComp
+    IMF_EXPORT
+    void addSource (DeepScanLineInputFile* file);
+
+    /////////////////////////////////////////
+    //
+    // set the frame buffer for output values
+    // the buffers specified must be large enough
+    // to handle the dataWindow()
+    //
+    /////////////////////////////////////////
+    IMF_EXPORT
+    void setFrameBuffer (const FrameBuffer& fr);
+
+    /////////////////////////////////////////
+    //
+    // retrieve frameBuffer
+    //
+    ////////////////////////////////////////
+    IMF_EXPORT
+    const FrameBuffer& frameBuffer () const;
+
+    //////////////////////////////////////////////////
+    //
+    // read scanlines start to end from the source(s)
+    // storing the result in the frame buffer provided
+    //
+    //////////////////////////////////////////////////
+
+    IMF_EXPORT
+    void readPixels (int start, int end);
+
+    IMF_EXPORT
+    int sources () const; // return number of sources
+
+    /////////////////////////////////////////////////
+    //
+    // retrieve the datawindow
+    // If multiple parts are specified, this will
+    // be the union of the dataWindow of all parts
+    //
+    ////////////////////////////////////////////////
+
+    IMF_EXPORT
+    const IMATH_NAMESPACE::Box2i& dataWindow () const;
+
+    //
+    // override default sorting/compositing operation
+    // (otherwise an instance of the base class will be used)
+    //
+
+    IMF_EXPORT
+    void setCompositing (DeepCompositing*);
+
+    struct IMF_HIDDEN Data;
+
+
+    //
+    // set the maximum number of samples that will be composited.
+    // If a single scanline has more samples, readPixels will throw
+    // an exception. This mechanism prevents the library allocating
+    // excessive memory to composite deep scanline images.
+    // A value of 0 will cause deep compositing to be disabled entirely
+    // A negative value disables the limit, allowing images with
+    // arbitrarily large sample counts to be composited
+    //
+    IMF_EXPORT
+    static void setMaximumSampleCount(int64_t sampleCount);
+
+    IMF_EXPORT
+    static int64_t getMaximumSampleCount();
+
+
+private:
+    struct Data* _Data;
+
+    CompositeDeepScanLine (const CompositeDeepScanLine&) = delete;
+    CompositeDeepScanLine& operator= (const CompositeDeepScanLine&) = delete;
+    CompositeDeepScanLine (CompositeDeepScanLine&&)                 = delete;
+    CompositeDeepScanLine& operator= (CompositeDeepScanLine&&) = delete;
+>>>>>>> 8587f4ee... Add maximumSampleCount limit to CompositeDeepScanLine (#1230)
 };
 
 OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_EXIT
