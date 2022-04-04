0d91a9a039d48d3c4dd9b8f00f40a25bfee23efa
Peter Hillman <peterh@wetafx.co.nz>
2022-04-04 16:37:22 +1200

    handle potential overflow in CheckFile core reading
    Signed-off-by: Peter Hillman <peterh@wetafx.co.nz>

--- src/lib/OpenEXRUtil/ImfCheckFile.cpp
+++ src/lib/OpenEXRUtil/ImfCheckFile.cpp
@@ -1555,7 +1555,7 @@ memstream_read (
     {
         memdata* md   = static_cast<memdata*> (userdata);
         uint64_t left = sz;
-        if ((offset + sz) > md->bytes)
+        if (offset > md->bytes ||  sz > md->bytes || offset+sz > md->bytes)
             left = (offset < md->bytes) ? md->bytes - offset : 0;
         if (left > 0) memcpy (buffer, md->data + offset, left);
         rdsz = static_cast<int64_t> (left);
