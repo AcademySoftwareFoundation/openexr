334e2138dad87088310dad21db7c160c535afa1a
Cary Phillips <cary@ilm.com>
2023-02-05 19:39:10 -0800

    Add missing license/copyright
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- docs/InterpretingDeepPixels.rst
+++ docs/InterpretingDeepPixels.rst
@@ -1,3 +1,7 @@
+..
+  SPDX-License-Identifier: BSD-3-Clause
+  Copyright (c) Contributors to the OpenEXR Project.
+
 Interpreting OpenEXR Deep Pixels
 ################################
 
--- src/lib/OpenEXR/b44ExpLogTable.h
+++ src/lib/OpenEXR/b44ExpLogTable.h
@@ -1,4 +1,9 @@
 //
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) DreamWorks Animation LLC and Contributors of the OpenEXR Project
+//
+
+//
 // This is an automatically generated file.
 // Do not edit.
 //
