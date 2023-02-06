e7a86ea0149f20dc133f183fbb66d6d57f5f5acc
Cary Phillips <cary@ilm.com>
2023-02-05 19:39:10 -0800

    Add missing license/copyright
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) Contributors to the OpenEXR Project.
+
 #Look for an executable called sphinx-build
 find_program(SPHINX_EXECUTABLE
              NAMES sphinx-build
--- docs/MultiViewOpenEXR.rst
+++ docs/MultiViewOpenEXR.rst
@@ -1,3 +1,7 @@
+..
+  SPDX-License-Identifier: BSD-3-Clause
+  Copyright (c) Contributors to the OpenEXR Project.
+
 Storing Multi-View Images in OpenEXR Files
 ##########################################
 
