c0883e1aa3142c2b07b9d431fb150a28adf19262
Cary Phillips <cary@ilm.com>
2023-03-15 08:53:55 -0700

    Fix run_gcov.sh to work with .c files
    Deduce the original source file by extracting it from the .o.d file
    rather than mindlessly replacing .gcno with .cpp, which misses .c
    files.
    
    Also, allow explicit build root as an optional argument.
    
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- share/ci/scripts/linux/run_gcov.sh
+++ share/ci/scripts/linux/run_gcov.sh
@@ -1,17 +1,39 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) Contributors to the OpenEXR Project.
 
 set -ex
 
-mkdir _coverage
-cd _coverage
+if [ $# -gt 0 ]
+then
+   build=$1  # use explicity-provided build directory
+else
+   build=`pwd`/'_build'  # from with CI, use the _build subdirectory
+fi
+   
+coverage="$build/_coverage"
 
-# The sed command below converts from:
-#   ../_build/src/OpenEXR/CMakeFiles/OpenColorIO.dir/ops/Exponent.gcno
-# to:
-#   ../src/OpenEXR/ops/Exponent.cpp
+mkdir -p $coverage
+cd $coverage
 
-for g in $(find ../_build -name "*.gcno" -type f); do
-    gcov -l -p -o $(dirname "$g") $(echo "$g" | sed -e 's/\/_build\//\//' -e 's/\.gcno/\.cpp/' -e 's/\/CMakeFiles.*\.dir\//\//')
+for gcno in $(find $build -name "*.gcno" -type f); do
+
+    # Identify the original source file (.cpp or .c) from the .gcno
+    # file by examining the .o.d make dependency file. The source
+    # file should be the second line in the .o.d file.
+    
+    # gcno = $build/src/bin/exrheader/CMakeFiles/exrheader.dir/main.gcno
+    # object_directory = $build/src/bin/exrheader/CMakeFiles/exrheader.dir
+    # source_base = $build/src/bin/exrheader/CMakeFiles/exrheader.dir/main
+    # dependenty_file = $build/src/bin/exrheader/CMakeFiles/exrheader.dir/main.o.d
+
+    object_directory=$(dirname "$gcno") 
+    source_base=$(basename "$gcno" ".gcno") 
+    dependency_file=$object_directory/$source_base.o.d 
+    source_file=$(head -2 $dependency_file | tail -1 | sed -e 's/ //' -e 's/ \\//') 
+    gcov -l -p -o $object_directory $source_file 
 done
 
-cd ..
+
+
