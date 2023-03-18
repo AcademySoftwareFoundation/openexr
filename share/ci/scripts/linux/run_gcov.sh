8395690e47d2e3772687a6c5db2d5e416f05e896
Cary Phillips <cary@ilm.com>
2023-03-17 21:04:46 -0700

    Fix run_gcov.sh to use proper _build/_coverage dirs
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- share/ci/scripts/linux/run_gcov.sh
+++ share/ci/scripts/linux/run_gcov.sh
@@ -8,12 +8,12 @@ set -ex
 if [ $# -gt 0 ]
 then
    build=$1  # use explicity-provided build directory
+   coverage="$build/_coverage"
 else
-   build=`pwd`/'_build'  # from with CI, use the _build subdirectory
+   build='../_build'  # from with CI, use the _build subdirectory
+   coverage="_coverage"
 fi
    
-coverage="$build/_coverage"
-
 mkdir -p $coverage
 cd $coverage
 
@@ -28,9 +28,9 @@ for gcno in $(find $build -name "*.gcno" -type f); do
     # source_base = $build/src/bin/exrheader/CMakeFiles/exrheader.dir/main
     # dependenty_file = $build/src/bin/exrheader/CMakeFiles/exrheader.dir/main.o.d
 
-    object_directory=$(dirname "$gcno") 
-    source_base=$(basename "$gcno" ".gcno") 
-    dependency_file=$object_directory/$source_base.o.d 
+    object_directory=$(dirname "$gcno")
+    source_base=$(basename "$gcno" ".gcno")
+    dependency_file=$object_directory/$source_base.o.d
 
     if [ -f "$dependency_file" ]; then
 
