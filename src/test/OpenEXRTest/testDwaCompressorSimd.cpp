7cfa661648c30aae6f1a2eb2c44401321420e02a
mandree <mandree@users.noreply.github.com>
2023-03-24 02:27:54 +0100

    Fix ARMv7 build for DwaCompressor, too. (#1368)
    To fix #1367.
    
    Signed-off-by: Matthias Andree <matthias.andree@gmx.de>

--- src/test/OpenEXRTest/testDwaCompressorSimd.cpp
+++ src/test/OpenEXRTest/testDwaCompressorSimd.cpp
@@ -401,7 +401,7 @@ testFloatToHalf()
         }
     }
 
-#ifdef IMF_HAVE_NEON
+#ifdef IMF_HAVE_NEON_AARCH64
     {
         cout << "      convertFloatToHalf64_neon()" << endl;
         for (int iter = 0; iter < numIter; ++iter)
@@ -430,7 +430,7 @@ testFloatToHalf()
             }
         }
     }
-    #endif // IMF_HAVE_NEON
+    #endif // IMF_HAVE_NEON_AARCH64
 }
 
 //
@@ -516,8 +516,8 @@ testFromHalfZigZag()
         } // iter
     }     // f16c
 
-#ifdef IMF_HAVE_NEON
-    {
+#ifdef IMF_HAVE_NEON_AARCH64
+     {
         const int            numIter = 1000000;
         Rand48               rand48 (0);
         half                 h;
@@ -553,7 +553,7 @@ testFromHalfZigZag()
         } // iter
     }     // neon
 
-#endif // IMF_HAVE_NEON
+#endif // IMF_HAVE_NEON_AARCH64
 }
 
 
