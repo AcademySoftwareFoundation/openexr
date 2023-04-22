f1580470a4c64ae99dceddec2bae2e1498c1d8e2
Nick Porcino <meshula@hotmail.com>
2023-04-21 22:28:12 -0700

    Govern library version by OpenEXRVersion.h
    Signed-off-by: Nick Porcino <meshula@hotmail.com>

---
+++ src/lib/OpenEXR/OpenEXRVersion.h
@@ -0,0 +1,10 @@
+//
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Contributors to the OpenEXR Project.
+//
+// This is the OpenEXR library version information.
+// ImfVersion.h contains version information about the file format.
+
+#define VERSION_MAJOR 3
+#define VERSION_MINOR 2
+#define VERSION_PATCH 0
