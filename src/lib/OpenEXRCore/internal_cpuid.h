0a8623a16a5be468cede07886723f19556039d7a
Kimball Thurston <kdt3rd@gmail.com>
2023-05-16 12:31:13 +1200

    Fix implementation of cpuid
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- src/lib/OpenEXRCore/internal_cpuid.h
+++ src/lib/OpenEXRCore/internal_cpuid.h
@@ -3,6 +3,8 @@
 ** Copyright Contributors to the OpenEXR Project.
 */
 
+#include "OpenEXRConfigInternal.h"
+
 #if defined(__x86_64__) || defined(_M_X64)
 #    ifndef _WIN32
 #        include <cpuid.h>
@@ -11,20 +13,34 @@
 
 static inline void check_for_x86_simd (int *f16c, int *avx, int *sse2)
 {
-#if (defined(__x86_64__) || defined(_M_X64))
-#    if defined(__AVX__) && defined(__F16C__)
+// Only use compiler flags on e2k (MCST Elbrus 2000) architecture
+#ifdef __e2k__
+#    if defined(__SSE2__)
+    *sse2 = 1;
+#    endif
+#    if defined(__AVX__)
+    *avx = 1;
+#    endif
+#    if defined(__F16C__)
+    *f16c = 1;
+#    endif
+
+#else
+#    if (defined(__x86_64__) || defined(_M_X64))
+
+#        if defined(__AVX__) && defined(__F16C__)
     *f16c = 1;
     *avx = 1;
     *sse2 = 1;
-#    else
-#        ifdef _WIN32
+#        else
+#            ifdef _WIN32
     int regs[4], osxsave;
 
     __cpuid (regs, 0);
     if (regs[0] >= 1) { __cpuidex (regs, 1, 0); }
     else
         regs[2] = 0;
-#        else
+#            else
     unsigned int regs[4], osxsave;
     __get_cpuid (0, &regs[0], &regs[1], &regs[2], &regs[3]);
     if (regs[0] >= 1)
@@ -33,7 +49,7 @@ static inline void check_for_x86_simd (int *f16c, int *avx, int *sse2)
     }
     else
         regs[2] = 0;
-#        endif
+#            endif
     /* AVX is indicated by bit 28, F16C by 29 of ECX (reg 2) */
     osxsave = (regs[2] & (1 << 27)) ? 1 : 0;
     *avx    = (regs[2] & (1 << 28)) ? 1 : 0;
@@ -49,7 +65,8 @@ static inline void check_for_x86_simd (int *f16c, int *avx, int *sse2)
     else
     {
         /* check extended control register */
-#        ifdef IMF_HAVE_GCC_INLINEASM_X86
+#            if defined(OPENEXR_IMF_HAVE_GCC_INLINE_ASM_AVX) &&                            \
+    (defined(_M_X64) || defined(__x86_64__))
         __asm__ __volatile__("xgetbv"
                              : /* Output  */ "=a"(regs[0]), "=d"(regs[3])
                              : /* Input   */ "c"(0)
@@ -60,16 +77,17 @@ static inline void check_for_x86_simd (int *f16c, int *avx, int *sse2)
             *avx  = 0;
             *f16c = 0;
         }
-#        else
+#            else
         *avx  = 0;
         *f16c = 0;
-#        endif
+#            endif
     }
-#    endif
-#else
+#        endif
+#    else
     *f16c = 0;
     *avx  = 0;
     *sse2 = 0;
+#    endif
 #endif
 }
 
