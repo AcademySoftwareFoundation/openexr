3e6ce3fe023956cff6c0733b87bf2fd420d7638a
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2023-07-11 17:20:03 +0200

    Simplified the test
    Signed-off-by: Jose Luis Cercos-Pita <jlcercos@gmail.com>

--- src/wrappers/python/tests/minimal.py
+++ src/wrappers/python/tests/minimal.py
@@ -1,14 +1,6 @@
-from unittest import TestCase, main as unittest_main
-import openexr
+import pytest
 
 
-class MinimalTests(TestCase):
-    def setUp(self):
-        pass
-
-    def test_one_iter(self):
-        print(openexr.__version__)
-
-
-if __name__ == '__main__':
-    unittest_main() 
+def test_import():
+    import openexr
+    print(openexr.__version__)
