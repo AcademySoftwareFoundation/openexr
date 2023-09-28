35a9f2ed696bd1c7ca46982c6e343c8da6b7ec04
Cary Phillips <cary@ilm.com>
2023-09-27 23:11:32 -0400

    Set EXR_DEFLATE_LIB to deflate_LINK_LIBRARIES
    deflate_LINK_LIBRARIES has the necessary -L flag needed by pkgconfig,
    whereas deflate_LIBRARIES is the bare library name.
    
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- cmake/OpenEXRSetup.cmake
+++ cmake/OpenEXRSetup.cmake
@@ -215,7 +215,7 @@ if(NOT TARGET PkgConfig::deflate AND NOT deflate_FOUND)
   set(EXR_DEFLATE_LIB)
 else()
   set(EXR_DEFLATE_INCLUDE_DIR)
-  set(EXR_DEFLATE_LIB ${deflate_LIBRARIES})
+  set(EXR_DEFLATE_LIB ${deflate_LINK_LIBRARIES})
   # set EXR_DEFATE_LDFLAGS for OpenEXR.pc.in for static build
   if (BUILD_SHARED_LIBS)
     set(EXR_DEFLATE_LDFLAGS "")
