dab9b430c9bee88590712d275e788d891e86846a
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2023-10-14 20:37:29 +0200

    The version was not read appropriately
    Signed-off-by: Jose Luis Cercos-Pita <jlcercos@gmail.com>

--- src/wrappers/python/set_version.py
+++ src/wrappers/python/set_version.py
@@ -2,9 +2,9 @@ version = ""
 with open('src/lib/OpenEXRCore/openexr_version.h', 'r') as f:
     txt = f.read()
     for name in ('MAJOR', 'MINOR', 'PATCH'):
-        prefix = 'set(OPENEXR_{}_VERSION '.format(name)
+        prefix = 'OPENEXR_{}_VERSION '.format(name)
         subtxt = txt[txt.find(prefix) + len(prefix):]
-        subtxt = subtxt[:subtxt.find(')\n')]
+        subtxt = subtxt[:subtxt.find('\n')]
         version = version + subtxt + '.'
     version = version[:-1]
 
