87fd8f6bc2af31e6677c8cd109778b3a99f18dd5
Philippe Leprince <github.pleprince@gmail.com>
2024-02-26 17:11:04 +0100

    Restore comments describing the codecs.
    Signed-off-by: Philippe Leprince <github.pleprince@gmail.com>

--- src/lib/OpenEXR/ImfCompression.h
+++ src/lib/OpenEXR/ImfCompression.h
@@ -9,8 +9,8 @@
 //-----------------------------------------------------------------------------
 //
 //  enum Compression
-// 
-// This file enumerates available compression methods and defines a simple API 
+//
+// This file enumerates available compression methods and defines a simple API
 // to query them.
 //
 //-----------------------------------------------------------------------------
@@ -24,17 +24,34 @@ OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_ENTER
 // NOTE: Must be extended to add a new codec.
 enum IMF_EXPORT_ENUM Compression
 {
-    NO_COMPRESSION    = 0,
-    RLE_COMPRESSION   = 1,
-    ZIPS_COMPRESSION  = 2,
-    ZIP_COMPRESSION   = 3,
-    PIZ_COMPRESSION   = 4,
-    PXR24_COMPRESSION = 5,
-    B44_COMPRESSION   = 6,
-    B44A_COMPRESSION  = 7,
-    DWAA_COMPRESSION  = 8,
-    DWAB_COMPRESSION  = 9,
-    NUM_COMPRESSION_METHODS
+    NO_COMPRESSION = 0, // no compression.
+
+    RLE_COMPRESSION = 1, // run length encoding.
+
+    ZIPS_COMPRESSION = 2, // zlib compression, one scan line at a time.
+
+    ZIP_COMPRESSION = 3, // zlib compression, in blocks of 16 scan lines.
+
+    PIZ_COMPRESSION = 4, // piz-based wavelet compression.
+
+    PXR24_COMPRESSION = 5, // lossy 24-bit float compression
+
+    B44_COMPRESSION = 6, // lossy 4-by-4 pixel block compression,
+                         // fixed compression rate.
+
+    B44A_COMPRESSION = 7, // lossy 4-by-4 pixel block compression,
+                          // flat fields are compressed more.
+
+    DWAA_COMPRESSION = 8, // lossy DCT based compression, in blocks
+                          // of 32 scanlines. More efficient for partial
+                          // buffer access.
+
+    DWAB_COMPRESSION = 9, // lossy DCT based compression, in blocks
+                          // of 256 scanlines. More efficient space
+                          // wise and faster to decode full frames
+                          // than DWAA_COMPRESSION.
+
+    NUM_COMPRESSION_METHODS // number of different compression methods.
 };
 
 /// Returns a codec ID's short name (lowercase).
