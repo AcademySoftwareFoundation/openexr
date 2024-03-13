b06ca2d9e4a31689116848658440910c5bbc774e
Philippe Leprince <github.pleprince@gmail.com>
2024-03-13 12:23:05 +0100

    Adopt new compression API
    Signed-off-by: Philippe Leprince <github.pleprince@gmail.com>

--- src/lib/OpenEXR/ImfZstdCompressor.h
+++ src/lib/OpenEXR/ImfZstdCompressor.h
@@ -13,16 +13,18 @@
 #include "vector"
 
 OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_ENTER
+
 class ZstdCompressor : public Compressor
 {
 public:
     explicit ZstdCompressor (const Header& hdr);
+
 private:
     using raw_ptr = std::unique_ptr<char, decltype (&free)>;
-    std::vector<raw_ptr>    _outBuffer;
-    int        numScanLines () const override; // max
-    int        compress (
-               const char* inPtr, int inSize, int minY, const char*& outPtr) override;
+    std::vector<raw_ptr> _outBuffer;
+    int                  numScanLines () const override; // max
+    int                  compress (
+                         const char* inPtr, int inSize, int minY, const char*& outPtr) override;
     int uncompress (
         const char* inPtr, int inSize, int minY, const char*& outPtr) override;
 };
