6776fbdc38c641854ff3493d7068f0573e91dfdf
Kimball Thurston <kdt3rd@gmail.com>
2024-05-06 21:45:10 +1200

    Move tables, test logic to core now compression is moved
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- src/test/OpenEXRTest/main.cpp
+++ src/test/OpenEXRTest/main.cpp
@@ -11,7 +11,6 @@
 #include "OpenEXRConfigInternal.h"
 
 #include "testAttributes.h"
-#include "testB44ExpLogTable.h"
 #include "testBackwardCompatibility.h"
 #include "testBadTypeAttributes.h"
 #include "testChannels.h"
@@ -29,7 +28,6 @@
 #include "testDeepScanLineHuge.h"
 #include "testDeepScanLineMultipleRead.h"
 #include "testDeepTiledBasic.h"
-#include "testDwaLookups.h"
 #include "testExistingStreams.h"
 #include "testFutureProofing.h"
 #include "testHeader.h"
@@ -229,8 +227,6 @@ main (int argc, char* argv[])
     TEST (testBackwardCompatibility, "core");
     TEST (testFutureProofing, "core");
     TEST (testRle, "core");
-    TEST (testB44ExpLogTable, "core");
-    TEST (testDwaLookups, "core");
     TEST (testIDManifest, "core");
     TEST (testCpuId, "core");
     TEST (testHeader, "basic");
