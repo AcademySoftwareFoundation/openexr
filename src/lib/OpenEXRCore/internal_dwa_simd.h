0eb5081487e4c72bd3a7791bc29cceaf3460573f
Kimball Thurston <kdt3rd@gmail.com>
2024-12-31 18:24:00 +1300

    windows does not have an x86intrin.h header
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- src/lib/OpenEXRCore/internal_dwa_simd.h
+++ src/lib/OpenEXRCore/internal_dwa_simd.h
@@ -22,7 +22,9 @@
 #    define IMF_HAVE_SSE2 1
 #    include <emmintrin.h>
 #    include <mmintrin.h>
-#    include <x86intrin.h>
+#    ifndef _MSC_VER
+#         include <x86intrin.h>
+#    endif
 #endif
 
 #if defined(__ARM_NEON)
