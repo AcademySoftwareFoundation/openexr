6e8e640fc164c8af7f4f60ac7021d4d3e13d60b8
Christopher Schwartz <christopherschwartz@xrite.com>
2025-02-13 09:03:30 +0100

    Change casted type to match the type of the assigned to variable on both, 32 and 64 bit platforms
    Signed-off-by: Christopher Schwartz <christopherschwartz@xrite.com>

--- src/lib/OpenEXRCore/chunk.c
+++ src/lib/OpenEXRCore/chunk.c
@@ -639,7 +639,7 @@ extract_chunk_table (
         }
         else { priv_to_native64 (ctable, part->chunk_count); }
 
-        nptr = (uintptr_t) ctable;
+        nptr = (uint64_t) ctable;
         // see if we win or not
         if (!atomic_compare_exchange_strong (
                 EXR_CONST_CAST (atomic_uintptr_t*, &(part->chunk_table)),
