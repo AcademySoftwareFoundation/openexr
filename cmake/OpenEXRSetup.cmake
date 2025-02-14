47e50ce70a093f200a4eb125564ba682750a659e
Rasmus Debitsch <rasmus.debitsch@faro.com>
2025-02-14 19:15:16 +0100

    fix_1981: Don't set the library postfix in the cmake cache. Otherwise it will set this variable if OpenEXR is a subproject

--- cmake/OpenEXRSetup.cmake
+++ cmake/OpenEXRSetup.cmake
@@ -89,7 +89,10 @@ set(CMAKE_INCLUDE_CURRENT_DIR ON)
 
 # Suffix for debug configuration libraries
 # (if you should choose to install those)
-set(CMAKE_DEBUG_POSTFIX "_d" CACHE STRING "Suffix for debug builds")
+# Don't override if the user has set it and don't save it in the cache
+if (NOT CMAKE_DEBUG_POSTFIX)
+  set(CMAKE_DEBUG_POSTFIX "_d")
+endif()
 
 if(NOT OPENEXR_IS_SUBPROJECT)
   # Usual cmake option to build shared libraries or not, only overridden if OpenEXR is a top level project,
