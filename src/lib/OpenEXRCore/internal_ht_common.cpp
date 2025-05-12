ff6b7666828f76020fa25e98cc3cff7bdc101053
Pierre-Anthony Lemieux <pal@sandflow.com>
2025-05-12 07:44:13 -0700

    Improve RGB channel detection heuristics for the HT256 encoder (#2029)
    * Allow lowercase r,g and b, and reg, greee and blue.
    
    Signed-off-by: Pierre-Anthony Lemieux <pal@palemieux.com>
    
    * Improved RGB channel detection heuristics
    
    Signed-off-by: Pierre-Anthony Lemieux <pal@palemieux.com>
    
    * Fix Windows build
    
    Signed-off-by: Pierre-Anthony Lemieux <pal@palemieux.com>
    
    * Improve RGB heuristics
    Add unit tests
    
    Signed-off-by: Pierre-Anthony Lemieux <pal@palemieux.com>
    
    * Removed unusued color channel abbreviations
    
    Signed-off-by: Pierre-Anthony Lemieux <pal@palemieux.com>
    
    ---------
    
    Signed-off-by: Pierre-Anthony Lemieux <pal@palemieux.com>

--- src/lib/OpenEXRCore/internal_ht_common.cpp
+++ src/lib/OpenEXRCore/internal_ht_common.cpp
@@ -6,34 +6,137 @@
 #include "internal_ht_common.h"
 #include <vector>
 #include <string>
+#include <string.h>
 #include <cassert>
+#include <algorithm>
+#include <cctype>
+
+const std::string RED_CH_FULLNAME = "red";
+const std::string GREEN_CH_FULLNAME = "green";
+const std::string BLUE_CH_FULLNAME = "blue";
+
+struct RGBChannelParams
+{
+    const char* r_suffix;
+    const char* g_suffix;
+    const char* b_suffix;
+    int         r_index;
+    int         g_index;
+    int         b_index;
+    const char* prefix;
+    size_t      prefix_len;
+};
+
+static inline bool areEqual(const char* a, const char* b) {
+#ifdef _MSC_VER
+    return _stricmp (a, b) == 0;
+#else
+    return strcasecmp (a, b) == 0;
+#endif
+}
 
 bool
 make_channel_map (
-    int channel_count, exr_coding_channel_info_t* channels, std::vector<CodestreamChannelInfo>& cs_to_file_ch)
+    int                                 channel_count,
+    exr_coding_channel_info_t*          channels,
+    std::vector<CodestreamChannelInfo>& cs_to_file_ch)
 {
-    int r_index = -1;
-    int g_index = -1;
-    int b_index = -1;
+    /** Heuristic detection of RGB channels so that the JPEG 2000 Reversible
+      * Color Transform (RCT), a decorrelation transform, can be applied.
+      *
+      * RGB channels are present if the either (a) the names of the channels in
+      * their entirety or (b) following the "." character match one of the
+      * triplets defined in {params}. Order of the channels and case of the
+      * channel names are ignored.
+      *
+      * Example 1: "main.b", "main.g", "main.r" match
+      * Example 2: "MainR", "MainG", "MainB" do not match
+      * Example 3: "R", "B", "B" match
+      * Example 4: "red", "green", "blue" match
+      */
 
-    cs_to_file_ch.resize(channel_count);
+    RGBChannelParams params[] = {
+        {"r", "g", "b", -1, -1, -1, NULL, 0},
+        {"red", "green", "blue", -1, -1, -1, NULL, 0}};
+    constexpr size_t params_count = sizeof (params) / sizeof (params[0]);
+
+    cs_to_file_ch.resize (channel_count);
 
     for (size_t i = 0; i < channel_count; i++)
     {
-        std::string c_name(channels[i].channel_name);
+        const char* channel_name = channels[i].channel_name;
+        const char* suffix       = strrchr (channel_name, '.');
+        const char* prefix       = channel_name;
+        size_t      prefix_len   = 0;
+        if (suffix) {
+            suffix += 1;
+            prefix_len = suffix - prefix - 1;
+        } else {
+            suffix = channel_name;
+        }
 
-        if (c_name == "R") { r_index = i; }
-        else if (c_name == "G") { g_index = i; }
-        else if (c_name == "B") { b_index = i; }
+        for (size_t j = 0; j < params_count; j++)
+        {
+            if (params[j].prefix != NULL &&
+                (params[j].prefix_len != prefix_len ||
+                 strncmp (params[j].prefix, prefix, params[j].prefix_len)))
+            {
+                /* skip to the next potential match if a prefix has already been
+                record and does not match the channel prefix */
+                continue;
+            }
+
+            bool match = false;
+            if (areEqual (suffix, params[j].r_suffix) &&
+                params[j].r_index < 0)
+            {
+                params[j].r_index = i;
+                match = true;
+            }
+            else if (
+                areEqual (suffix, params[j].g_suffix) &&
+                params[j].g_index < 0)
+            {
+                params[j].g_index = i;
+                match = true;
+            }
+            else if (
+                areEqual (suffix, params[j].b_suffix) &&
+                params[j].b_index < 0)
+            {
+                params[j].b_index = i;
+                match = true;
+            }
+
+            if (match) {
+                /* record the prefix if one is not already recorded and move to
+                the next channel */
+                params[j].prefix = prefix;
+                params[j].prefix_len = prefix_len;
+                break;
+            }
+        }
     }
 
-    bool isRGB = r_index >= 0 && g_index >= 0 && b_index >= 0 &&
-                 channels[r_index].data_type == channels[g_index].data_type &&
-                 channels[r_index].data_type == channels[b_index].data_type &&
-                 channels[r_index].x_samples == channels[g_index].x_samples &&
-                 channels[r_index].x_samples == channels[b_index].x_samples &&
-                 channels[r_index].y_samples == channels[g_index].y_samples &&
-                 channels[r_index].y_samples == channels[b_index].y_samples;
+    int  r_index;
+    int  g_index;
+    int  b_index;
+    bool isRGB = false;
+
+    for (size_t j = 0; (!isRGB) && j < params_count; j++)
+    {
+        r_index = params[j].r_index;
+        g_index = params[j].g_index;
+        b_index = params[j].b_index;
+
+        isRGB = r_index > -1 && g_index > -1 && b_index > -1 &&
+                channels[r_index].data_type == channels[g_index].data_type &&
+                channels[r_index].data_type == channels[b_index].data_type &&
+                channels[r_index].x_samples == channels[g_index].x_samples &&
+                channels[r_index].x_samples == channels[b_index].x_samples &&
+                channels[r_index].y_samples == channels[g_index].y_samples &&
+                channels[r_index].y_samples == channels[b_index].y_samples;
+    }
 
     if (isRGB)
     {
