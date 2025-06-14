9282464403bbd2a4900623b7c6764e908d7ee663
Cary Phillips <cary@ilm.com>
2025-06-14 12:59:24 -0700

    Add comments to the install scripts
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- share/ci/scripts/install_imath.sh
+++ share/ci/scripts/install_imath.sh
@@ -2,10 +2,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright Contributors to the OpenColorIO Project.
 
+# This script is used by the ci_workflow.yml/ci_steps.yml CI to
+# install Imath. This is a part of the process that validates that
+# OpenEXR's cmake properly locates the Imath dependency, either
+# finding this installation or fetching the library from github to
+# build internally.
+
 set -ex
 
 TAG="$1"
 
+# The sudo is nececessary since the installation goes to /usr/local.
 SUDO=$(command -v sudo >/dev/null 2>&1 && echo sudo || echo "")
 
 git clone https://github.com/AcademySoftwareFoundation/Imath.git
