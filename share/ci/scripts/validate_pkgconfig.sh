e1d2d45c327539830a2e22d5f37cd071afd8098f
Cary Phillips <cary@ilm.com>
2025-08-29 17:19:13 -0700

    Remove openjph from OpenEXR.pc
    It's not a part of the public interface, so it's not necessary for
    application.
    
    Also, since src/lib/OpenEXRCore/CMakeLists.txt declares OpenJPH as
    private:
    
     target_link_libraries(OpenEXRCore PRIVATE ${EXR_OPENJPH_LIB})
    
    it is *not* necessary in the `target_link_libraries(PUBLIC)`
    declaration in cmake/LibraryDefine.cmake, so this change removes it
    there.
    
    This also adds a ci test to validate the OpenEXR.pc.
    
    Signed-off-by: Cary Phillips <cary@ilm.com>

---
+++ share/ci/scripts/validate_pkgconfig.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright Contributors to the OpenEXR Project.
+
+#
+# Validate that the given source file compiles and links with the
+# given pkg-config file.
+#
+# First argument is the directory where Imath is installed
+# Second argument is the pkg name, i.e. Imath or PyImath
+# Third argument is the test program source 
+#
+
+set -x
+
+if [ "$#" -ne 2 ]; then
+    echo "Error: Expected 2 arguments, got $#." >&2
+    echo "Usage: $0 <install dir> <src file>" >&2
+    exit 1
+fi
+
+INSTALL=$1
+SRC=$2
+
+LIB="lib"
+if [ -d "$INSTALL/lib64" ]; then
+    LIB="lib64"
+fi
+
+export PKG_CONFIG_PATH="$INSTALL/$LIB/pkgconfig:/usr/local/$LIB/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
+
+cflags=$(pkg-config --cflags --libs OpenEXR Imath openjph libdeflate)
+bin=$SRC.bin
+g++ -std=c++17 $SRC $cflags -o $bin -Wl,-rpath,$INSTALL/$LIB:/usr/local/$LIB
+./$bin
