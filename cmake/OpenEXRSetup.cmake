eae3b31358c4a39ab7cd4c85ab2ecea87fce8476
Cary Phillips <cary@ilm.com>
2025-10-30 16:12:01 -0600

    Only define CMAKE_DEBUG_POSTFIX if it is not already defined (#2166)
    This makes it possible to set CMAKE_DEBUG_POSTFIX="", which wasn't
    possible before but is desirable in the case where the debug libraries
    should have the same name as the release build.
    
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- cmake/OpenEXRSetup.cmake
+++ cmake/OpenEXRSetup.cmake
@@ -100,7 +100,7 @@ set(CMAKE_INCLUDE_CURRENT_DIR ON)
 # Suffix for debug configuration libraries
 # (if you should choose to install those)
 # Don't override if the user has set it and don't save it in the cache
-if (NOT CMAKE_DEBUG_POSTFIX)
+if (NOT DEFINED CMAKE_DEBUG_POSTFIX)
   set(CMAKE_DEBUG_POSTFIX "_d")
 endif()
 
