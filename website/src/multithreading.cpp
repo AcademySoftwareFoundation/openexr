46ff2e9bc2deb5e580f30a1ba20c719eb83a3b2d
Cary Phillips <cary@ilm.com>
2025-11-13 12:03:31 -0500

    Add copywrite notices to website example code
    The copywrite notices should appear in the files but not on the
    website pages, so the `.. literalinclude::` blocks need a ` lines: 5-`
    so it skips the 5 lines of the copywrite notice and renders only the
    following lines.
    
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- website/src/multithreading.cpp
+++ website/src/multithreading.cpp
@@ -1,3 +1,8 @@
+//
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Contributors to the OpenEXR Project.
+//
+
 char fileName[] = "";
 // [begin main thread create]
 // main, before application threads are created:
