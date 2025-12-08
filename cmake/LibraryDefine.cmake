81136170cb8ff5f17f8e49fb49a0eacb0fd08410
Kimball Thurston <kdt3rd@gmail.com>
2025-12-09 00:38:29 +1300

    Add option to compile c++ libraries with an embedded core library
    Can configure with cmake -DOPENEXR_FORCE_EMBEDDED_CORE=ON
    
    This will force symbols to be hidden in the Core library such that when
    linked into the c++ shared library or other binary, they will be hidden
    (as if there is no core library). This should not be the default config
    but is supported for creating custom versions of exr.
    
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- cmake/LibraryDefine.cmake
+++ cmake/LibraryDefine.cmake
@@ -4,7 +4,7 @@
 # NB: This function has a number of Imath-specific names/variables
 # in it, so be careful copying...
 function(OPENEXR_DEFINE_LIBRARY libname)
-  set(options)
+  set(options EMBEDDED)
   set(oneValueArgs PRIV_EXPORT CURDIR CURBINDIR)
   set(multiValueArgs SOURCES HEADERS DEPENDENCIES PRIVATE_DEPS)
   cmake_parse_arguments(OPENEXR_CURLIB "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
@@ -13,7 +13,12 @@ function(OPENEXR_DEFINE_LIBRARY libname)
     set(_openexr_extra_flags "$<$<COMPILE_LANGUAGE:CXX>:/EHsc>" "$<$<COMPILE_LANGUAGE:CXX>:/MP>")
   endif()
   set(objlib ${libname})
-  add_library(${objlib}
+  if(OPENEXR_CURLIB_EMBEDDED)
+    set(libopts STATIC)
+  else()
+    set(libopts)
+  endif()
+  add_library(${objlib} ${libopts}
     ${OPENEXR_CURLIB_HEADERS}
     ${OPENEXR_CURLIB_SOURCES})
 
@@ -24,7 +29,9 @@ function(OPENEXR_DEFINE_LIBRARY libname)
                           PRIVATE cxx_std_${OPENEXR_CXX_STANDARD}
                           INTERFACE cxx_std_17 )
 
-  if(OPENEXR_CURLIB_PRIV_EXPORT AND BUILD_SHARED_LIBS)
+  if(OPENEXR_CURLIB_EMBEDDED)
+    set(libopts)
+  elseif(OPENEXR_CURLIB_PRIV_EXPORT AND BUILD_SHARED_LIBS)
     target_compile_definitions(${objlib} PRIVATE ${OPENEXR_CURLIB_PRIV_EXPORT})
     if(WIN32)
       target_compile_definitions(${objlib} PUBLIC OPENEXR_DLL)
@@ -46,7 +53,7 @@ function(OPENEXR_DEFINE_LIBRARY libname)
     CXX_EXTENSIONS OFF
     POSITION_INDEPENDENT_CODE ON
   )
-  if (NOT OPENEXR_USE_DEFAULT_VISIBILITY)
+  if (OPENEXR_CURLIB_EMBEDDED OR NOT OPENEXR_USE_DEFAULT_VISIBILITY)
     set_target_properties(${objlib} PROPERTIES
       C_VISIBILITY_PRESET hidden
       CXX_VISIBILITY_PRESET hidden
@@ -60,7 +67,7 @@ function(OPENEXR_DEFINE_LIBRARY libname)
   endif()
   set_property(TARGET ${objlib} PROPERTY PUBLIC_HEADER ${OPENEXR_CURLIB_HEADERS})
 
-  if(BUILD_SHARED_LIBS)
+  if(BUILD_SHARED_LIBS AND NOT OPENEXR_CURLIB_EMBEDDED)
     set_target_properties(${libname} PROPERTIES
       SOVERSION ${OPENEXR_LIB_SOVERSION}
       VERSION ${OPENEXR_LIB_VERSION}
@@ -70,7 +77,6 @@ function(OPENEXR_DEFINE_LIBRARY libname)
       OUTPUT_NAME "${libname}${OPENEXR_LIB_SUFFIX}"
       RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
   )
-  add_library(${PROJECT_NAME}::${libname} ALIAS ${libname})
 
   if(OPENEXR_INSTALL)
     install(TARGETS ${libname}
@@ -83,7 +89,9 @@ function(OPENEXR_DEFINE_LIBRARY libname)
         DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${OPENEXR_OUTPUT_SUBDIR}
     )
   endif()
-  if(BUILD_SHARED_LIBS AND (NOT "${OPENEXR_LIB_SUFFIX}" STREQUAL "") AND NOT WIN32)
+  if(OPENEXR_CURLIB_EMBEDDED)
+    set(libopts)
+  elseif(BUILD_SHARED_LIBS AND (NOT "${OPENEXR_LIB_SUFFIX}" STREQUAL "") AND NOT WIN32)
     string(TOUPPER "${CMAKE_BUILD_TYPE}" uppercase_CMAKE_BUILD_TYPE)
     set(verlibname ${CMAKE_SHARED_LIBRARY_PREFIX}${libname}${OPENEXR_LIB_SUFFIX}${CMAKE_${uppercase_CMAKE_BUILD_TYPE}_POSTFIX}${CMAKE_SHARED_LIBRARY_SUFFIX})
     set(baselibname ${CMAKE_SHARED_LIBRARY_PREFIX}${libname}${CMAKE_${uppercase_CMAKE_BUILD_TYPE}_POSTFIX}${CMAKE_SHARED_LIBRARY_SUFFIX})
@@ -93,4 +101,6 @@ function(OPENEXR_DEFINE_LIBRARY libname)
     set(verlibname)
     set(baselibname)
   endif()
+
+  add_library(${PROJECT_NAME}::${libname} ALIAS ${libname})
 endfunction()
