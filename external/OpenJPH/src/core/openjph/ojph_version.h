e81e55b4b72f0fcd568c58d43ba643cbf76ccdf4
Cary Phillips <cary@ilm.com>
2026-02-23 11:55:53 -0800

    Vendor OpenJPH v0.26.3 (#2250)
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- external/OpenJPH/src/core/openjph/ojph_version.h
+++ external/OpenJPH/src/core/openjph/ojph_version.h
@@ -34,5 +34,5 @@
 //***************************************************************************/
 
 #define OPENJPH_VERSION_MAJOR 0
-#define OPENJPH_VERSION_MINOR 25
+#define OPENJPH_VERSION_MINOR 26
 #define OPENJPH_VERSION_PATCH 3
