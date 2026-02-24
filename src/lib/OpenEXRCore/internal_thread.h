229db1fc2eb8cc463d9f6b823f7371e3f79665fe
Cary Phillips <cary@ilm.com>
2026-02-23 17:44:05 -0800

    simply internal_thread.h #if logic
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/lib/OpenEXRCore/internal_thread.h
+++ src/lib/OpenEXRCore/internal_thread.h
@@ -8,34 +8,10 @@
 
 #include "openexr_config.h"
 
-// Thread-safe single initiatization, using C11 threads if available,
-// InitOnceExecuteOnce on Windows, pthread_once elsewhere, or a simple
-// variable if threading is completely disabled.
-//
-// C11 threads.h is available when __STDC_NO_THREADS__ is not defined
-// (C11+) or when the system provides it (e.g., glibc 2.43+).
-
+// Thread-safe single initiatization, using InitOnceExecuteOnce on Windows,
+// pthread_once elsewhere, or a simple variable if threading is completely disabled.
 #if ILMTHREAD_THREADING_ENABLED
-
-// Check for C11 threads.h availability.
-// Exclude MSVC - its threads.h has compatibility issues with C++ compilation.
-#    if !defined(_MSC_VER) && !defined(_WIN32)
-#        if defined(__has_include)
-#            if __has_include(<threads.h>)
-#                define EXR_HAS_C11_THREADS 1
-#            endif
-#        endif
-// Also check via __STDC_NO_THREADS__ (defined when threads.h is NOT available)
-#        if !defined(EXR_HAS_C11_THREADS) && defined(__STDC_VERSION__) && \
-            __STDC_VERSION__ >= 201112L && !defined(__STDC_NO_THREADS__)
-#            define EXR_HAS_C11_THREADS 1
-#        endif
-#    endif
-
-#    if defined(EXR_HAS_C11_THREADS)
-#        include <threads.h>
-// C11 provides once_flag, ONCE_FLAG_INIT, and call_once - use them directly
-#    elif defined(_WIN32)
+#    ifdef _WIN32
 #        include <windows.h>
 #        define ONCE_FLAG_INIT INIT_ONCE_STATIC_INIT
 typedef INIT_ONCE once_flag;
@@ -51,7 +27,7 @@ call_once (once_flag* flag, void (*func) (void))
 {
     InitOnceExecuteOnce (flag, once_init_fn, (PVOID) func, NULL);
 }
-#    else
+#    elif !defined(ONCE_FLAG_INIT)
 #        include <pthread.h>
 #        define ONCE_FLAG_INIT PTHREAD_ONCE_INIT
 typedef pthread_once_t once_flag;
@@ -61,8 +37,11 @@ call_once (once_flag* flag, void (*func) (void))
     (void) pthread_once (flag, func);
 }
 #    endif
-#else
-#    define ONCE_FLAG_INIT 0
+
+#else /* !ILMTHREAD_THREADING_ENABLED */
+
+#    ifndef ONCE_FLAG_INIT
+#        define ONCE_FLAG_INIT 0
 typedef int once_flag;
 static inline void
 call_once (once_flag* flag, void (*func) (void))
@@ -72,6 +51,8 @@ call_once (once_flag* flag, void (*func) (void))
         func ();
     }
 }
-#endif
+#    endif
+
+#endif /* ILMTHREAD_THREADING_ENABLED */
 
 #endif /* OPENEXR_PRIVATE_THREAD_H */
