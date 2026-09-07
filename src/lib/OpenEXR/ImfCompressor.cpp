fe89594e2e91cb5619bec97c27f8d4313e4b96e5
Kimball Thurston <kdt3rd@gmail.com>
2026-09-07 18:19:55 +1200

    Update src/lib/OpenEXR/ImfCompressor.cpp
    Normalize return value usage
    
    Co-authored-by: Cary Phillips <seabeepea@gmail.com>
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- src/lib/OpenEXR/ImfCompressor.cpp
+++ src/lib/OpenEXR/ImfCompressor.cpp
@@ -390,7 +390,8 @@ newCompressor (Compression c, size_t maxScanLineSize, const Header& hdr)
         case HTJ2K256_COMPRESSION:
         case LJ2K_COMPRESSION:
 
-            return new HTCompressor (hdr, static_cast<int> (maxScanLineSize), scans);
+            ret = new HTCompressor (hdr, static_cast<int> (maxScanLineSize), scans);
+            break;
 
         case HTJ2K32_COMPRESSION:
 
